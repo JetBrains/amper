8a36ae6419463bee50eed9c5e8396609df089f8d
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2023-03-16 12:17:33 +0100

    Present Platform as a natural KMP hierarchy

--- prototype-implementation/frontend-api/src/main/kotlin/org/jetbrains/deft/proto/frontend/platform.kt
+++ prototype-implementation/frontend-api/src/main/kotlin/org/jetbrains/deft/proto/frontend/platform.kt
@@ -1,35 +1,52 @@
 package org.jetbrains.deft.proto.frontend
 
 /**
- * Enum, that describes the set of platform specific attributes that platform combines.
- */
-enum class PlatformFamily(val parent: PlatformFamily? = null) {
-    // Not sure for now, if backend needs that much granularity.
-    // Will remove if unused, with [parent] field.
-    NATIVE,
-    IOS(NATIVE),
-    ANDROID,
-    JVM,
-    JS,
-}
-
-/**
  * Enum, that describes the concrete platform the sources are built for.
+ *
+ * [parent] — Parent of the platform in natural KMP hierarchy.
+ * [isLeaf] — Helper flag to indicate that the platform is a leaf in the hierarchy.
  */
-enum class Platform(
-    val family: PlatformFamily
-) {
-    // ios
-    IOS_ARM_X64(PlatformFamily.IOS),
-    IOS_ARM_SIMULATOR(PlatformFamily.IOS),
-    IOS_X_64(PlatformFamily.IOS),
+enum class Platform(val parent: Platform? = null, val isLeaf: Boolean = false) {
+    COMMON,
 
-    // android
-    ANDROID(PlatformFamily.ANDROID),
+    JS(COMMON),
+    JVM(COMMON),
+    WASM(COMMON),
+    ANDROID(COMMON),
+    NATIVE(COMMON),
 
-    // jvm
-    JVM(PlatformFamily.JVM),
+    LINUX(NATIVE),
+    LINUX_X64(LINUX, isLeaf = true),
+    LINUX_ARM64(LINUX, isLeaf = true),
 
-    // js
-    JS(PlatformFamily.JS),
+    APPLE(NATIVE),
+
+    TVOS(APPLE),
+    TVOS_ARM64(TVOS, isLeaf = true),
+    TVOS_X64(TVOS, isLeaf = true),
+    TVOS_SIMULATOR_ARM64(TVOS, isLeaf = true),
+
+    MACOS(APPLE),
+    MACOS_X64(MACOS, isLeaf = true),
+    MACOS_ARM64(MACOS, isLeaf = true),
+
+    IOS(APPLE),
+    IOS_ARM64(IOS, isLeaf = true),
+    IOS_SIMULATOR_ARM64(IOS, isLeaf = true),
+    IOS_X64(IOS, isLeaf = true),
+
+    WATCHOS(APPLE),
+    WATCHOS_ARM64(WATCHOS),
+    WATCHOS_ARM32(WATCHOS),
+    WATCHOS_DEVICE_ARM64(WATCHOS, isLeaf = true),
+    WATCHOS_SIMULATOR_ARM64(WATCHOS, isLeaf = true),
+
+    MINGW(NATIVE),
+    MINGW_X64(MINGW, isLeaf = true),
+
+    ANDROID_NATIVE(NATIVE),
+    ANDROID_NATIVE_ARM32(ANDROID_NATIVE, isLeaf = true),
+    ANDROID_NATIVE_ARM64(ANDROID_NATIVE, isLeaf = true),
+    ANDROID_NATIVE_X64(ANDROID_NATIVE, isLeaf = true),
+    ANDROID_NATIVE_X86(ANDROID_NATIVE, isLeaf = true),
 }
