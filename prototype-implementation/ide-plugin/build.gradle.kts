2e220d5d5f7cdd2bca6ba1d7b90d6f8fa57a3b74
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2023-04-20 14:48:26 +0200

    Setup ide-plugin

---
+++ prototype-implementation/ide-plugin/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.gradleIntelliJPlugin)
+}
+
+group = "org.jetbrains.deft.ide"
+version = properties("ide-plugin.version")
+
+fun properties(key: String) = providers.gradleProperty(key)
+fun env(key: String) = providers.environmentVariable(key)
+
+kotlin {
+    jvmToolchain(11)
+}
+
+intellij {
+    pluginName = properties("ide-plugin.name")
+    version = properties("ide-plugin.platform.version")
+    type = properties("ide-plugin.platform.type")
+    plugins = properties("ide-plugin.platform.plugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+}
+
+tasks {
+    patchPluginXml {
+        version = properties("ide-plugin.version")
+        sinceBuild = properties("ide-plugin.compatibility.since-build")
+        untilBuild = properties("ide-plugin.compatibility.until-build")
+    }
+}
--- prototype-implementation/settings.gradle.kts
+++ prototype-implementation/settings.gradle.kts
@@ -1,8 +1,14 @@
-rootProject.name = "prototype-implementation"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
+
 include(
     "frontend-api",
     "frontend:util",
     "gradle-integration",
     "frontend:fragments:yaml",
     "frontend:plain:yaml",
+    "ide-plugin"
 )
+
+rootProject.name = "prototype-implementation"
