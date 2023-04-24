d1fe1a67711955cd0a95330d1d38539e0c4bfeeb
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2023-04-24 12:16:12 +0200

    Rename frontend subprojects
    As we decided to call fragmentless design as plain and there is no more need to call fragments design subproject as `with-fragments`

--- prototype-implementation/settings.gradle.kts
+++ prototype-implementation/settings.gradle.kts
@@ -1,6 +1,8 @@
 rootProject.name = "prototype-implementation"
-include("frontend-api")
-include("frontend:util")
-include("frontend:with-fragments:yaml")
-include("gradle-integration")
-include("frontend:without-fragments:yaml")
+include(
+    "frontend-api",
+    "frontend:util",
+    "gradle-integration",
+    "frontend:fragments:yaml",
+    "frontend:plain:yaml",
+)
