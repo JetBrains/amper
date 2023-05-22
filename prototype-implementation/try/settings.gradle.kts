dcf374e65e95d5755920d0f8ba0b2a11824e33e0
Alexander.Tsarev <alexander.tsarev@jetbrains.com>
2023-05-22 15:14:23 +0200

    Reduce declaration of gradle plugin usage to a minimum in the try project.

--- prototype-implementation/try/settings.gradle.kts
+++ prototype-implementation/try/settings.gradle.kts
@@ -1,23 +1,12 @@
-buildscript {
+pluginManagement {
     repositories {
-        maven("https://jitpack.io")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+//        maven("https://packages.jetbrains.team/maven/p/deft/scratch")
         mavenLocal()
         mavenCentral()
         google()
-        jcenter()
-        gradlePluginPortal()
-    }
-
-    dependencies {
-        classpath("org.jetbrains.deft.proto.frontend:frontend-api:1.0-SNAPSHOT")
-        // Uncomment this to use without fragments.
-        classpath("org.jetbrains.deft.proto.frontend.without-fragments.yaml:yaml:1.0-SNAPSHOT")
-        // Uncomment this to use with fragments.
-//        classpath("org.jetbrains.deft.proto.frontend.with-fragments.yaml:yaml:1.0-SNAPSHOT")
-        classpath("org.jetbrains.deft.proto.settings.plugin:gradle-integration:1.0-SNAPSHOT")
-        classpath("org.jetbrains.kotlin.multiplatform:org.jetbrains.kotlin.multiplatform.gradle.plugin:1.8.10")
     }
 }
 
-plugins.apply("org.jetbrains.deft.proto.settings.plugin")
+plugins {
+    id("org.jetbrains.deft.proto.settings.plugin") version "1.0-SNAPSHOT"
+}
