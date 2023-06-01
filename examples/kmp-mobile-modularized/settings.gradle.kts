645da6fbac95a20b4c3c2fff2367ee41cf0493b8
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2023-06-01 17:27:31 +0200

    Switch gradle plugin in example to version from space packages

--- examples/kmp-mobile-modularized/settings.gradle.kts
+++ examples/kmp-mobile-modularized/settings.gradle.kts
@@ -1,22 +1,34 @@
 buildscript {
+    val localProperties = java.util.Properties().apply {
+        val stream = rootDir.resolve("../../prototype-implementation/root.local.properties")
+            .takeIf { it.exists() }
+            ?.inputStream()
+        if (stream != null) load(stream)
+    }
     repositories {
         maven("https://jitpack.io")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-        mavenLocal()
+//        mavenLocal()
         mavenCentral()
         google()
         jcenter()
         gradlePluginPortal()
+        maven {
+            name = "spacePackages"
+            url = java.net.URI.create("https://packages.jetbrains.team/maven/p/deft/scratch")
+            credentials {
+                username = localProperties.getProperty("scratch.username")
+                    ?: error("No \"scratch.username\" in root.local.properties!")
+                password = localProperties.getProperty("scratch.password")
+                    ?: error("No \"scratch.password\" in root.local.properties!")
+            }
+        }
     }
 
     dependencies {
-        classpath("org.jetbrains.deft.proto.frontend:frontend-api:1.0-SNAPSHOT")
-        // Uncomment this to use without fragments.
-        classpath("org.jetbrains.deft.proto.frontend.without-fragments.yaml:yaml:1.0-SNAPSHOT")
-        // Uncomment this to use with fragments.
-//        classpath("org.jetbrains.deft.proto.frontend.with-fragments.yaml:yaml:1.0-SNAPSHOT")
-        classpath("org.jetbrains.deft.proto.settings.plugin:gradle-integration:1.0-SNAPSHOT")
-        classpath("org.jetbrains.kotlin.multiplatform:org.jetbrains.kotlin.multiplatform.gradle.plugin:1.8.10")
+//        classpath("org.jetbrains.deft.proto.settings.plugin:gradle-integration:1.0-SNAPSHOT")
+        classpath("org.jetbrains.deft.proto.settings.plugin:gradle-integration:1.1.1")
+        classpath("org.jetbrains.kotlin.multiplatform:org.jetbrains.kotlin.multiplatform.gradle.plugin:1.8.21")
     }
 }
 
