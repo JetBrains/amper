9525c923073b2fac14c5fbd00a5a776811d0b48d
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2023-07-19 13:14:11 +0200

    Refactor E2E tests so that they run consistently

--- prototype-implementation/e2e-test/src/E2eTest.kt
+++ prototype-implementation/e2e-test/src/E2eTest.kt
@@ -5,119 +5,78 @@ import kotlin.test.assertContains
 
 class E2eTest {
     @Test
-    fun `new project template runs and prints Hello, World`() {
-        // get test resources folder
-        val file = Path.of("../../examples/new-project-template")
-        // run gradle
-        val buildResult = GradleRunner.create()
-            .withProjectDir(file.toFile())
-            .withArguments("run", "--stacktrace")
-            .build()
-
-        assertContains(buildResult.output, "Hello, World!")
-    }
+    fun `new project template runs and prints Hello, World`() = test(
+        exampleName = "new-project-template",
+        "run",
+        expectOutputToHave = "Hello, World!",
+    )
 
     @Test
-    fun `jvm-hello-world runs and prints Hello, World`() {
-        // get test resources folder
-        val file = Path.of("../../examples/jvm-hello-world")
-        // run gradle
-        val buildResult = GradleRunner.create()
-            .withProjectDir(file.toFile())
-            .withArguments("run", "--stacktrace")
-            .build()
-
-        assertContains(buildResult.output, "Hello, World!")
-    }
+    fun `jvm-hello-world runs and prints Hello, World`() = test(
+        exampleName = "jvm-hello-world",
+        "run",
+        expectOutputToHave = "Hello, World!",
+    )
 
     @Test
-    fun `jvm-android-hello-world assembles`() {
-        // get test resources folder
-        val file = Path.of("../../examples/jvm+android-hello-world")
-        // run gradle
-        val buildResult = GradleRunner.create()
-            .withProjectDir(file.toFile())
-            .withArguments("assemble")
-            .withDebug(true)
-            .build()
-
-        assertContains(buildResult.output, "BUILD SUCCESSFUL")
-    }
+    fun `jvm-android-hello-world assembles`() = test(
+        exampleName = "jvm+android-hello-world",
+        "assemble",
+        expectOutputToHave = "BUILD SUCCESSFUL",
+    )
 
     @Test
-    fun `jvm-kotlin+java runs and prints Hello, World`() {
+    fun `jvm-kotlin+java runs and prints Hello, World`() = test(
+        exampleName = "jvm-kotlin+java",
+        "run",
+        expectOutputToHave = "Hello, World",
+    )
+
+    @Test
+    fun `jvm-with-tests test task fails`() = test(
+        exampleName = "jvm-with-tests",
+        "test",
+        expectOutputToHave = "java.lang.AssertionError at WorldTest.kt:13",
+        shouldSucceed = false,
+    )
+
+    @Test
+    fun `kmp-mobile test task succeeds`() = test(
+        exampleName = "kmp-mobile",
+        "test",
+        expectOutputToHave = "BUILD SUCCESSFUL",
+    )
+
+    @Test
+    fun `kmp-mobile-modularized test task succeeds`() = test(
+        exampleName = "kmp-mobile-modularized",
+        "test",
+        expectOutputToHave = "BUILD SUCCESSFUL",
+    )
+
+    @Test
+    fun `variants builds`() = test(
+        exampleName = "build-variants",
+        "build",
+        expectOutputToHave = "BUILD SUCCESSFUL",
+    )
+
+    private fun test(
+        exampleName: String,
+        vararg buildArguments: String,
+        expectOutputToHave: String,
+        shouldSucceed: Boolean = true,
+    ) {
         // given
-        val file = Path.of("../../examples/jvm-kotlin+java")
+        val file = Path.of("../../examples/$exampleName")
 
         // when
-        val buildResult = GradleRunner.create()
+        val runner = GradleRunner.create()
             .withProjectDir(file.toFile())
-            .withArguments("run", "--stacktrace")
-            .build()
+            .withArguments(*buildArguments, "--stacktrace")
+        val buildResult = if (shouldSucceed) runner.build() else runner.buildAndFail()
 
         // then
-        assertContains(buildResult.output, "Hello, World")
-    }
-
-    @Test
-    fun `jvm-with-tests test task fails`() {
-        // given
-        val file = Path.of("../../examples/jvm-with-tests")
-
-        // when
-        @Suppress("UnstableApiUsage") val buildResult = GradleRunner.create()
-            .withProjectDir(file.toFile())
-            .withArguments("test", "--stacktrace")
-            .run()
-
-        // then
-        assertContains(buildResult.output, "java.lang.AssertionError at WorldTest.kt:13")
-    }
-
-    @Test
-    fun `kmp-mobile test task fails`() {
-        // given
-        val file = Path.of("../../examples/kmp-mobile")
-
-        // when
-        @Suppress("UnstableApiUsage") val buildResult = GradleRunner.create()
-            .withProjectDir(file.toFile())
-            .withArguments("test", "--stacktrace")
-            .withDebug(true)
-            .run()
-
-        // then
-        assertContains(buildResult.output, "BUILD SUCCESSFUL")
-    }
-
-    @Test
-    fun `kmp-mobile-modularized test task fails`() {
-        // given
-        val file = Path.of("../../examples/kmp-mobile-modularized")
-
-        // when
-        @Suppress("UnstableApiUsage") val buildResult = GradleRunner.create()
-            .withProjectDir(file.toFile())
-            .withArguments("test", "--stacktrace")
-            .withDebug(true)
-            .run()
-
-        // then
-        assertContains(buildResult.output, "BUILD SUCCESSFUL")
-    }
-
-    @Test
-    fun `variants builds`() {
-        // given
-        val file = Path.of("../../examples/build-variants")
-
-        // when
-        val buildResult = GradleRunner.create()
-            .withProjectDir(file.toFile())
-            .withArguments("build", "--stacktrace")
-            .withDebug(true)
-            .build()
-        // then
-        assertContains(buildResult.output, "BUILD SUCCESSFUL")
+        assertContains(buildResult.output, expectOutputToHave)
     }
 }
