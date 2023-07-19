03f1c8dc36b9397220150aa36c2482e2a7fbae89
Anton Makeev <Anton.Makeev@jetbrains.com>
2023-07-19 17:57:20 +0200

    also sync gradle distribution

--- syncVersions.kts
+++ syncVersions.kts
@@ -1,21 +1,28 @@
 import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.io.path.PathWalkOption
+import kotlin.io.path.exists
 import kotlin.io.path.name
 import kotlin.io.path.walk
 
 val versions = mapOf(
-    "org.jetbrains.deft.proto.settings.plugin:gradle-integration" to "1.2.6",
-    "org.jetbrains.kotlin.multiplatform:org.jetbrains.kotlin.multiplatform.gradle.plugin" to "1.9.20-dev-6845"
+    "org.jetbrains.deft.proto.settings.plugin:gradle-integration:" to "1.2.6",
+    "org.jetbrains.kotlin.multiplatform:org.jetbrains.kotlin.multiplatform.gradle.plugin:" to "1.9.20-dev-6845",
+    "https\\://services.gradle.org/distributions/gradle-" to "8.1.1-bin.zip"
 )
 
 updateFiles(Path.of("examples"), versions)
-updateFiles(Path.of("gradle-integration/settings.gradle.kts"), versions)
+updateFiles(Path.of("prototype-implementation/settings.gradle.kts"), versions)
+updateFiles(Path.of("prototype-implementation/gradle/wrapper/gradle-wrapper.properties"), versions)
 
 @OptIn(kotlin.io.path.ExperimentalPathApi::class)
 fun updateFiles(path: Path, versions: Map<String, String>) {
+    if (!path.exists()) error("Not found: $path")
+
     path.walk(PathWalkOption.BREADTH_FIRST).forEach {
-        if (it.name == "settings.gradle.kts" || it.name.endsWith(".yaml")) {
+        if (it.name == "settings.gradle.kts"
+            || it.name.endsWith(".yaml")
+            || it.name.endsWith(".properties")) {
             updateFile(it, versions)
         }
     }
@@ -26,7 +33,7 @@ fun updateFile(file: Path, versions: Map<String, String>) {
     var updated = original
     
     versions.forEach { dependency, version ->
-        val regex = Regex("(.*)(${Regex.escape(dependency)}:)([\\w\\.\\-]*)(.*)")
+        val regex = Regex("(.*)(${Regex.escape(dependency)})([\\w\\.\\-]*)(.*)")
         updated = updated.replace(regex, "$1$2$version$4")
     }
 
