ae2a016a0159b3bd9b1b403be9f35e5c0f3d65c2
Anton Makeev <Anton.Makeev@jetbrains.com>
2023-07-21 12:39:58 +0200

    update examples

--- examples/compose-desktop/src/main.kt
+++ examples/compose-desktop/src/main.kt
@@ -1,6 +1,31 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main(args: Array<String>) = application {
-    Window(onCloseRequest = ::exitApplication) {}
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            BasicText("Hello, Deft!")
+        }
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
 }
