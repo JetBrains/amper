8d3a982ad9193b4d509822dae2705efffc7c1844
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2023-07-25 16:18:16 +0200

    Revert back settings.gradle.kts in examples (new-project-template, modularized)

--- examples/new-project-template/settings.gradle.kts
+++ examples/new-project-template/settings.gradle.kts
@@ -1,25 +1,18 @@
-pluginManagement {
-    includeBuild("../../prototype-implementation")
-}
-plugins {
-    id("org.jetbrains.deft.proto.settings.plugin")
+buildscript {
+    repositories {
+        maven("https://jitpack.io")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        mavenCentral()
+        google()
+        jcenter()
+        gradlePluginPortal()
+        maven("https://packages.jetbrains.team/maven/p/deft/deft-prototype")
+    }
+
+    dependencies {
+        // !!! Use syncVersions.kts to update these versions
+        classpath("org.jetbrains.deft.proto.settings.plugin:gradle-integration:1.2.13")
+    }
 }
 
-//buildscript {
-//    repositories {
-//        maven("https://jitpack.io")
-//        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-//        mavenCentral()
-//        google()
-//        jcenter()
-//        gradlePluginPortal()
-//        maven("https://packages.jetbrains.team/maven/p/deft/deft-prototype")
-//    }
-//
-//    dependencies {
-//        // !!! Use syncVersions.kts to update these versions
-//        classpath("org.jetbrains.deft.proto.settings.plugin:gradle-integration:1.2.13")
-//    }
-//}
-//
-//plugins.apply("org.jetbrains.deft.proto.settings.plugin")
+plugins.apply("org.jetbrains.deft.proto.settings.plugin")
