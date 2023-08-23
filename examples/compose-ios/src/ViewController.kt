b7281b2aedced7f2dcd59fbc54de010c14f124d8
Anton Makeev <Anton.Makeev@jetbrains.com>
2023-08-23 08:40:16 +0200

    [docs] make iOS Compose example consistent with desktop and Android

--- examples/compose-ios/src/ViewController.kt
+++ examples/compose-ios/src/ViewController.kt
@@ -1,12 +1,22 @@
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.ComposeUIViewController
 
 @Composable
 fun App() {
-    Button({}) {
-        Text("Hello")
+
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            BasicText("Hello, World!")
+        }
     }
 }
 
