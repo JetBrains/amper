94b3fcd9808e0f98f4917268aab0199719696f9a
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2023-09-15 18:37:53 +0200

    [diagnostics] Rewrite product and platform parsing into using error reporting

--- prototype-implementation/frontend/plain/yaml/src/org/jetbrains/deft/proto/frontend/nodes/problemReporting.kt
+++ prototype-implementation/frontend/plain/yaml/src/org/jetbrains/deft/proto/frontend/nodes/problemReporting.kt
@@ -1,10 +1,31 @@
 package org.jetbrains.deft.proto.frontend.nodes
 
 import org.jetbrains.deft.proto.core.messages.ProblemReporterContext
+import org.jetbrains.deft.proto.frontend.BuildFileAware
 import org.jetbrains.deft.proto.frontend.FrontendYamlBundle
 import java.nio.file.Path
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
+
+inline fun <reified YamlNodeT : YamlNode?> nodeType(): String = when (YamlNodeT::class) {
+    YamlNode.Scalar::class -> "string"
+    YamlNode.Sequence::class -> "list"
+    YamlNode.Mapping::class -> "map"
+    else -> YamlNodeT::class.simpleName ?: "null"
+}
+
+context(BuildFileAware, ProblemReporterContext)
+@OptIn(ExperimentalContracts::class)
+inline fun <reified YamlNodeT : YamlNode?> YamlNode?.castOrReport(lazyElementName: () -> String): Boolean {
+    contract {
+        returns(true) implies (this@castOrReport is YamlNodeT)
+    }
+
+    return castOrReport<YamlNodeT>(
+        buildFile,
+        lazyElementName
+    )
+}
 
 context(ProblemReporterContext)
 @OptIn(ExperimentalContracts::class)
@@ -18,22 +39,40 @@ inline fun <reified YamlNodeT : YamlNode?> YamlNode?.castOrReport(
 
     if (this is YamlNodeT) return true
 
-    val expected = when (YamlNodeT::class) {
-        YamlNode.Scalar::class -> "string"
-        YamlNode.Sequence::class -> "list"
-        YamlNode.Mapping::class -> "map"
-        else -> YamlNodeT::class.simpleName
-    }
-
     problemReporter.reportError(
         FrontendYamlBundle.message(
             "wrong.element.type",
             lazyElementName(),
-            expected ?: "null",
+            nodeType<YamlNodeT>(),
             this?.nodeType ?: "null"
         ),
         file = file,
         line = (this?.startMark?.line ?: 0) + 1
     )
     return false
+}
+
+context(BuildFileAware, ProblemReporterContext)
+inline fun <reified ElementT : YamlNode> YamlNode.Sequence.getListOfElementsOrNull(lazyElementName: () -> String): List<ElementT>? =
+    getListOfElementsOrNull(buildFile, lazyElementName)
+
+context(ProblemReporterContext)
+inline fun <reified ElementT : YamlNode> YamlNode.Sequence.getListOfElementsOrNull(
+    file: Path,
+    lazyElementName: () -> String,
+): List<ElementT>? {
+    if (elements.any { it !is ElementT }) {
+        problemReporter.reportError(
+            FrontendYamlBundle.message(
+                "wrong.element.list.type",
+                lazyElementName(),
+                nodeType<ElementT>(),
+                elements.firstOrNull { it !is ElementT }?.nodeType ?: "null"
+            ),
+            file = file,
+            line = (startMark.line) + 1
+        )
+        return null
+    }
+    return elements.filterIsInstance<ElementT>()
 }
