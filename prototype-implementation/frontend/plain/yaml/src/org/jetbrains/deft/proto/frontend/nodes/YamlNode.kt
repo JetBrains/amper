750953619d80018dd5c729bd29ac47f13b133795
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2023-09-15 13:16:56 +0200

    [diagnostics] Extract YamlNode.Mapping helpers

--- prototype-implementation/frontend/plain/yaml/src/org/jetbrains/deft/proto/frontend/nodes/YamlNode.kt
+++ prototype-implementation/frontend/plain/yaml/src/org/jetbrains/deft/proto/frontend/nodes/YamlNode.kt
@@ -1,7 +1,5 @@
 package org.jetbrains.deft.proto.frontend.nodes
 
-import org.jetbrains.deft.proto.frontend.Settings
-
 data class Mark(val line: Int, val column: Int) {
     companion object {
         val Unknown = Mark(0, 0)
@@ -35,16 +33,3 @@ val YamlNode.nodeType: String
         is YamlNode.Sequence -> "list"
         is YamlNode.Mapping -> "map"
     }
-
-operator fun YamlNode.Mapping.get(key: String): YamlNode? =
-    mappings.firstNotNullOfOrNull { (k, v) -> if (k is YamlNode.Scalar && k.value == key) v else null }
-
-fun YamlNode.Mapping.toSettings(): Settings = mappings.mapNotNull { (k, v) ->
-    if (k is YamlNode.Scalar) k.value to v.convert() else null
-}.toMap()
-
-private fun YamlNode.convert(): Any = when (this) {
-    is YamlNode.Scalar -> value
-    is YamlNode.Sequence -> elements.map { it.convert() }
-    is YamlNode.Mapping -> mappings.associate { (k, v) -> k.convert() to v.convert() }
-}
