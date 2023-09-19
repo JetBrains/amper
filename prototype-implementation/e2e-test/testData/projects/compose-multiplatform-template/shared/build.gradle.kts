240eb032be15ae241f3c05477ecfc2cda11b7342
Ilia Solovei <ilia.solovei@jetbrains.com>
2023-09-19 19:46:46 +0200

    Clean-up in shared module

--- prototype-implementation/e2e-test/testData/projects/compose-multiplatform-template/shared/build.gradle.kts
+++ prototype-implementation/e2e-test/testData/projects/compose-multiplatform-template/shared/build.gradle.kts
@@ -7,78 +7,16 @@ plugins {
 }
 
 kotlin {
-//    androidTarget()
-//
-////    jvm("desktop")
-//    jvm()
-//
-//    listOf(
-//        iosX64(),
-//        iosArm64(),
-//        iosSimulatorArm64()
-//    ).forEach { iosTarget ->
-//        iosTarget.binaries.framework {
-//            baseName = "shared"
-//            isStatic = true
-//        }
-//    }
     targets.withType(KotlinNativeTarget::class.java).configureEach { // WA for integration with iosApp
         binaries.framework {
             baseName = "shared"
             isStatic = true
         }
     }
-
-    sourceSets {
-        val commonMain by getting {
-//            dependencies {
-//                implementation(compose.runtime)
-//                implementation(compose.foundation)
-//                implementation(compose.material)
-//                implementation(compose.components.resources)
-//            }
-        }
-        val androidMain by getting {
-//            dependencies {
-//                api("androidx.activity:activity-compose:1.7.2")
-//                api("androidx.appcompat:appcompat:1.6.1")
-//                api("androidx.core:core-ktx:1.10.1")
-//            }
-        }
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-//        val desktopMain by getting { //previously
-        val jvmMain by getting { //need to be jvmMain because deft don't support target renaming
-            dependencies {
-//                implementation(compose.desktop.common)
-            }
-        }
-    }
 }
 
 android {
-//    compileSdk = (findProperty("android.compileSdk") as String).toInt()
-//    namespace = "com.myapplication.common"
-
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
     sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
-//    defaultConfig {
-//        minSdk = (findProperty("android.minSdk") as String).toInt()
-//    }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_17
-//        targetCompatibility = JavaVersion.VERSION_17
-//    }
-//    kotlin {
-//        jvmToolchain(17)
-//    }
 }
