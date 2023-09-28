9921f3ac1ab8ff0ead30b5c766a90534f99d2fc7
Anton Makeev <Anton.Makeev@jetbrains.com>
2023-09-28 17:52:32 +0200

    DEFT-145 Rename Pot to module: rename `pot:` section (update examples)

--- examples/compose-ios/settings.gradle.kts
+++ examples/compose-ios/settings.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 
     dependencies {
         // !!! Use syncVersions.kts to update these versions
-        classpath("org.jetbrains.deft.proto.settings.plugin:gradle-integration:169-NIGHTLY")
+        classpath("org.jetbrains.deft.proto.settings.plugin:gradle-integration:170-NIGHTLY")
     }
 }
 
--- examples/gradle-migration-kmp/settings.gradle.kts
+++ examples/gradle-migration-kmp/settings.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.deft.proto.settings.plugin:gradle-integration:169-NIGHTLY")
+        classpath("org.jetbrains.deft.proto.settings.plugin:gradle-integration:170-NIGHTLY")
     }
 }
 // apply the plugin:
