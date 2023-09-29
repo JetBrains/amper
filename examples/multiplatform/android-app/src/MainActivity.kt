dfe1851fa72c7fd5c8f4b8a3de2bcdfaa64170b9
Anton Makeev <Anton.Makeev@jetbrains.com>
2023-09-29 17:06:39 +0200

    [examples] multiplatform: extract common Compose code

--- examples/multiplatform/android-app/src/MainActivity.kt
+++ examples/multiplatform/android-app/src/MainActivity.kt
@@ -1,31 +1,9 @@
 package hello.world
 
-import World
+import Screen
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.text.BasicText
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-
-
-@Composable
-fun Screen() {
-    MaterialTheme() {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            BasicText("Hello, ${World().get()}!")
-        }
-    }
-}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
