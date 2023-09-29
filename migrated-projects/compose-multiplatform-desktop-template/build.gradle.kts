f877198e55bb679475912869cb0167af004c863e
Anton Makeev <Anton.Makeev@jetbrains.com>
2023-09-29 13:18:31 +0200

    DEFT-43 Convert some KMP samples to Deft: compose-multiplatform-desktop-template

---
+++ migrated-projects/compose-multiplatform-desktop-template/build.gradle.kts
@@ -0,0 +1,13 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+compose.desktop {
+// https://youtrack.jetbrains.com/issue/DEFT-147/ Gradle interop: error when applying compose.desctop.application block
+//    application {
+//        mainClass = "MainKt"
+//        nativeDistributions {
+//            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+//            packageName = "KotlinJvmComposeDesktopApplication"
+//            packageVersion = "1.0.0"
+//        }
+//    }
+}
