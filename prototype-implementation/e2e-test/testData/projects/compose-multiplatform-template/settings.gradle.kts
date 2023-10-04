d77fadd801e75dc563ddf9d26e633424f116baad
Ilia Solovei <ilia.solovei@jetbrains.com>
2023-10-04 18:18:21 +0200

    migrated the desktopApp

--- prototype-implementation/e2e-test/testData/projects/compose-multiplatform-template/settings.gradle.kts
+++ prototype-implementation/e2e-test/testData/projects/compose-multiplatform-template/settings.gradle.kts
@@ -28,7 +28,7 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
-    id("org.jetbrains.deft.proto.settings.plugin").version("179-NIGHTLY")
+    id("org.jetbrains.deft.proto.settings.plugin").version("181-NIGHTLY")
 }
 
 dependencyResolutionManagement {
