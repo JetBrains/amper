b8e1f4a57948b7663097e3f0988c86dbdf90dac9
Ilia Solovei <ilia.solovei@jetbrains.com>
2023-10-04 21:01:21 +0200

    fixed run of all apps

--- prototype-implementation/e2e-test/testData/projects/compose-multiplatform-template/desktopApp/build.gradle.kts
+++ prototype-implementation/e2e-test/testData/projects/compose-multiplatform-template/desktopApp/build.gradle.kts
@@ -1,30 +1,11 @@
-//import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-//
-//plugins {
-//    kotlin("multiplatform")
-//    id("org.jetbrains.compose")
-//}
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
-//kotlin {
-//    jvm()
-//    sourceSets {
-//        val jvmMain by getting  {
-//            dependencies {
-//                implementation(compose.desktop.currentOs)
-//                implementation(project(":shared"))
-//            }
-//        }
-//    }
-//}
-
-//compose.desktop {
-//    application {
-//        mainClass = "MainKt"
-//
-//        nativeDistributions {
-//            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-//            packageName = "KotlinMultiplatformComposeDesktopApplication"
-//            packageVersion = "1.0.0"
-//        }
-//    }
-//}
+compose.desktop {
+    application {
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "KotlinMultiplatformComposeDesktopApplication"
+            packageVersion = "1.0.0"
+        }
+    }
+}
