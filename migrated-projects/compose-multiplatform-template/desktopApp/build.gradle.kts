720c614fb5d97f71543dc1cb8cd5fc20a21dd93d
Ilia Solovei <ilia.solovei@jetbrains.com>
2023-10-05 13:48:24 +0200

    Changed the layout for desktopApp and fixed saved run configuration to be able to run the app

--- migrated-projects/compose-multiplatform-template/desktopApp/build.gradle.kts
+++ migrated-projects/compose-multiplatform-template/desktopApp/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 compose.desktop {
     application {
-        mainClass = "MainKt"
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "KotlinMultiplatformComposeDesktopApplication"
