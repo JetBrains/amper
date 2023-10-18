18375359562a94eaef9394a44396e2dab73c4d51
Alexander.Tsarev <alexander.tsarev@jetbrains.com>
2023-10-18 14:25:14 +0200

    Change reports dependency to allTests.

--- prototype-implementation/build.gradle.kts
+++ prototype-implementation/build.gradle.kts
@@ -1,11 +1,11 @@
 val aggregateTestReports by tasks.creating(Zip::class) {
-    dependsOn(getTasksByName("jvmTest", true))
+    dependsOn(getTasksByName("allTests", true))
     destinationDirectory.set(buildDir)
     archiveBaseName.set("aggregatedTestReport")
 
     subprojects {
         this@subprojects.buildDir
-            .resolve("reports/tests/jvmTest")
+            .resolve("reports/tests/allTests")
             .takeIf { it.exists() }
             ?.let {
                 from(it) {
