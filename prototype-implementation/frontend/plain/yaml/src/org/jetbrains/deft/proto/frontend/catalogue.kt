4b00f00f9d3437fc0ad066ce24d0aca7348047cc
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2023-10-20 11:48:09 +0200

    DEFT-166 use '$compose.' instead of '$compose.dependencies.' in catalogs

--- prototype-implementation/frontend/plain/yaml/src/org/jetbrains/deft/proto/frontend/catalogue.kt
+++ prototype-implementation/frontend/plain/yaml/src/org/jetbrains/deft/proto/frontend/catalogue.kt
@@ -45,30 +45,30 @@ object BuiltInCatalog : PredefinedCatalog({
     // TODO Pass version from build.
     // Add compose.
     val composeVersion = "1.5.10-beta01"
-    put("compose.dependencies.animation", "org.jetbrains.compose.animation:animation:$composeVersion")
-    put("compose.dependencies.animationGraphics", "org.jetbrains.compose.animation:animation-graphics:$composeVersion")
-    put("compose.dependencies.foundation", "org.jetbrains.compose.foundation:foundation:$composeVersion")
-    put("compose.dependencies.material", "org.jetbrains.compose.material:material:$composeVersion")
-    put("compose.dependencies.material3", "org.jetbrains.compose.material3:material3:$composeVersion")
-    put("compose.dependencies.runtime", "org.jetbrains.compose.runtime:runtime:$composeVersion")
-    put("compose.dependencies.runtimeSaveable", "org.jetbrains.compose.runtime:runtime-saveable:$composeVersion")
-    put("compose.dependencies.ui", "org.jetbrains.compose.ui:ui:$composeVersion")
-    put("compose.dependencies.uiTooling", "org.jetbrains.compose.ui:ui-tooling:$composeVersion")
-    put("compose.dependencies.preview", "org.jetbrains.compose.ui:ui-tooling-preview:$composeVersion")
-    put("compose.dependencies.materialIconsExtended", "org.jetbrains.compose.material:material-icons-extended:$composeVersion")
-    put("compose.dependencies.components.resources", "org.jetbrains.compose.components:components-resources:$composeVersion")
-    put("compose.dependencies.html.svg", "org.jetbrains.compose.html:html-svg:$composeVersion")
-    put("compose.dependencies.html.testUtils", "org.jetbrains.compose.html:html-test-utils:$composeVersion")
-    put("compose.dependencies.html.core", "org.jetbrains.compose.html:html-core:$composeVersion")
-    put("compose.dependencies.desktop.components.splitPane", "org.jetbrains.compose.components:components-splitpane:$composeVersion")
-    put("compose.dependencies.desktop.components.animatedImage", "org.jetbrains.compose.components:components-animatedimage:$composeVersion")
-    put("compose.dependencies.desktop.common", "org.jetbrains.compose.desktop:desktop:$composeVersion")
-    put("compose.dependencies.desktop.linux_x64", "org.jetbrains.compose.desktop:desktop-jvm-linux-x64:$composeVersion")
-    put("compose.dependencies.desktop.linux_arm64", "org.jetbrains.compose.desktop:desktop-jvm-linux-arm64:$composeVersion")
-    put("compose.dependencies.desktop.windows_x64", "org.jetbrains.compose.desktop:desktop-jvm-windows-x64:$composeVersion")
-    put("compose.dependencies.desktop.macos_x64", "org.jetbrains.compose.desktop:desktop-jvm-macos-x64:$composeVersion")
-    put("compose.dependencies.desktop.macos_arm64", "org.jetbrains.compose.desktop:desktop-jvm-macos-arm64:$composeVersion")
-    put("compose.dependencies.desktop.uiTestJUnit4", "org.jetbrains.compose.ui:ui-test-junit4:$composeVersion")
+    put("compose.animation", "org.jetbrains.compose.animation:animation:$composeVersion")
+    put("compose.animationGraphics", "org.jetbrains.compose.animation:animation-graphics:$composeVersion")
+    put("compose.foundation", "org.jetbrains.compose.foundation:foundation:$composeVersion")
+    put("compose.material", "org.jetbrains.compose.material:material:$composeVersion")
+    put("compose.material3", "org.jetbrains.compose.material3:material3:$composeVersion")
+    put("compose.runtime", "org.jetbrains.compose.runtime:runtime:$composeVersion")
+    put("compose.runtimeSaveable", "org.jetbrains.compose.runtime:runtime-saveable:$composeVersion")
+    put("compose.ui", "org.jetbrains.compose.ui:ui:$composeVersion")
+    put("compose.uiTooling", "org.jetbrains.compose.ui:ui-tooling:$composeVersion")
+    put("compose.preview", "org.jetbrains.compose.ui:ui-tooling-preview:$composeVersion")
+    put("compose.materialIconsExtended", "org.jetbrains.compose.material:material-icons-extended:$composeVersion")
+    put("compose.components.resources", "org.jetbrains.compose.components:components-resources:$composeVersion")
+    put("compose.html.svg", "org.jetbrains.compose.html:html-svg:$composeVersion")
+    put("compose.html.testUtils", "org.jetbrains.compose.html:html-test-utils:$composeVersion")
+    put("compose.html.core", "org.jetbrains.compose.html:html-core:$composeVersion")
+    put("compose.desktop.components.splitPane", "org.jetbrains.compose.components:components-splitpane:$composeVersion")
+    put("compose.desktop.components.animatedImage", "org.jetbrains.compose.components:components-animatedimage:$composeVersion")
+    put("compose.desktop.common", "org.jetbrains.compose.desktop:desktop:$composeVersion")
+    put("compose.desktop.linux_x64", "org.jetbrains.compose.desktop:desktop-jvm-linux-x64:$composeVersion")
+    put("compose.desktop.linux_arm64", "org.jetbrains.compose.desktop:desktop-jvm-linux-arm64:$composeVersion")
+    put("compose.desktop.windows_x64", "org.jetbrains.compose.desktop:desktop-jvm-windows-x64:$composeVersion")
+    put("compose.desktop.macos_x64", "org.jetbrains.compose.desktop:desktop-jvm-macos-x64:$composeVersion")
+    put("compose.desktop.macos_arm64", "org.jetbrains.compose.desktop:desktop-jvm-macos-arm64:$composeVersion")
+    put("compose.desktop.uiTestJUnit4", "org.jetbrains.compose.ui:ui-test-junit4:$composeVersion")
 }) {
 
     private val systemInfo = DefaultSystemInfo
@@ -76,7 +76,7 @@ object BuiltInCatalog : PredefinedCatalog({
     context(ProblemReporterContext)
     override fun findInCatalogue(key: String, node: YamlNode) = when (key) {
         // Handle os detection as compose do.
-        "compose.dependencies.desktop.currentOs" ->
+        "compose.desktop.currentOs" ->
             "org.jetbrains.compose.desktop:desktop-jvm-${systemInfo.detect().familyArch}".asDeftSuccess()
 
         else ->
