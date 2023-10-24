012bfb80d6b07fe90a6853574834ed60a8674879
Alexander.Tsarev <alexander.tsarev@jetbrains.com>
2023-10-24 16:51:31 +0200

    DEFT-144 Change test to check embed and sign task.

--- prototype-implementation/e2e-test/test/E2ETestFixture.kt
+++ prototype-implementation/e2e-test/test/E2ETestFixture.kt
@@ -13,6 +13,7 @@ open class E2ETestFixture(val pathToProjects: String) {
         expectOutputToHave: String,
         shouldSucceed: Boolean = true,
         checkForWarnings: Boolean = true,
+        additionalEnv: Map<String, String> = emptyMap(),
         additionalCheck: (Path) -> Unit = {},
     ) {
         test(
@@ -21,6 +22,7 @@ open class E2ETestFixture(val pathToProjects: String) {
             listOf(expectOutputToHave),
             shouldSucceed,
             checkForWarnings,
+            additionalEnv,
             additionalCheck,
         )
     }
@@ -32,13 +34,16 @@ open class E2ETestFixture(val pathToProjects: String) {
         expectOutputToHave: Collection<String>,
         shouldSucceed: Boolean = true,
         checkForWarnings: Boolean = true,
+        additionalEnv: Map<String, String> = emptyMap(),
         additionalCheck: (Path) -> Unit = {},
     ) {
         val tempDir = prepareTempDirWithProject(projectName)
+        val newEnv = System.getenv().toMutableMap().apply { putAll(additionalEnv) }
         try {
             val runner = GradleRunner.create()
                 .withPluginClasspath()
                 .withProjectDir(tempDir.toFile())
+                .withEnvironment(newEnv)
 //                .withDebug(true)
                 .withArguments(*buildArguments, "--stacktrace")
             val buildResult = if (shouldSucceed) runner.build() else runner.buildAndFail()
