0420631bd826b4b940c1cd37336af4e6caffef89
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2023-10-30 14:52:57 +0100

    update copyright headers

--- prototype-implementation/core/test/org/jetbrains/amper/core/messages/ProblemReporterRenderingTest.kt
+++ prototype-implementation/core/test/org/jetbrains/amper/core/messages/ProblemReporterRenderingTest.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 package org.jetbrains.amper.core.messages
 
 import org.junit.jupiter.api.Assertions.assertEquals
