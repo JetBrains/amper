1e2d6b5d120f3e0c89465c00b5aa2da4fc57f0c6
utsmannn <utsmannn@gmail.com>
2023-11-22 18:06:39 +0700

    fix: adjust start.sh index and write local.properties

--- start.sh
+++ start.sh
@@ -12,15 +12,22 @@ for dir in $HOME/.amper-jb/examples/*; do
     ((index++))
 done
 
-read -p "Please select template available from Amper repository: " index
+read -p "Please select template available from Amper repository: " indexTemplate
 read -p "Type project name: " input_project_name
 
-result=${TEMPLATES[${index+1}]}
+result=${TEMPLATES[indexTemplate-1]}
 
 project_name_underscore=$(echo "$input_project_name" | sed 's/ /_/g')
 project_name_final_string=$(echo "$project_name_underscore" | tr '[:upper:]' '[:lower:]')
 
 mkdir $project_name_final_string
-mv "$result"/* "$project_name_final_string"
+mv "$result"/* "$project_name_final_string" && wait
 
 fleet "$project_name_final_string"
+
+if [[ -d "$HOME/Library/Android/sdk" ]]; then
+    echo "sdk.dir=$HOME/Android/sdk" >> "$project_name_final_string/local.properties"
+else
+    echo "sdk.dir=" >> "$project_name_final_string/local.properties"
+    echo "Android SDK not detect on your system. Please write manually in local.properties"
+fi
