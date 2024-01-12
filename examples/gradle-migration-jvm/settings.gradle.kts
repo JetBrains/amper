f0ddaf4041f9740d48fefe3f8fab8b7df0010844
Space Automation <no-reply@automation.jetbrains.space>
2024-01-12 12:15:10 +0000

    Update Amper plugin version to 0.2.0-dev-388

--- examples/gradle-migration-jvm/settings.gradle.kts
+++ examples/gradle-migration-jvm/settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 
 plugins {
     // apply the plugin:
-    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-387")
+    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-388")
 }
 
 rootProject.name = "my-project-name"
--- migrated-projects/compose-multiplatform-ios-android-template/settings.gradle.kts
+++ migrated-projects/compose-multiplatform-ios-android-template/settings.gradle.kts
@@ -10,5 +10,5 @@ pluginManagement {
 
 plugins {
     // apply the plugin:
-    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-387")
+    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-388")
 }
