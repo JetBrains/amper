bd68fcf65b1ff3abda6ddb46c2c11556371458b7
Space Automation <no-reply@automation.jetbrains.space>
2024-01-16 19:38:37 +0000

    Update Amper plugin version to 0.2.0-dev-395

--- migrated-projects/KaMPKit/settings.gradle.kts
+++ migrated-projects/KaMPKit/settings.gradle.kts
@@ -8,5 +8,5 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-393")
+    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-395")
 }
--- migrated-projects/compose-multiplatform-template/settings.gradle.kts
+++ migrated-projects/compose-multiplatform-template/settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 
 plugins {
     // apply the plugin:
-    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-393")
+    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-395")
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
 }
 
