8a7242ee00e2ec6388a9746246abbc761aed8dad
Space Automation <no-reply@automation.jetbrains.space>
2024-01-25 12:02:00 +0000

    Update Amper plugin version to 0.2.0-dev-410

--- examples/modularized/settings.gradle.kts
+++ examples/modularized/settings.gradle.kts
@@ -10,5 +10,5 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-409")
+    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-410")
 }
