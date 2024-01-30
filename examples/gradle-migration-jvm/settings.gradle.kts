4f8b92770a496e426f8e15b5fa56d1f486a3a5d4
Space Automation <no-reply@automation.jetbrains.space>
2024-01-30 18:40:35 +0000

    Update Amper plugin version to 0.2.0-dev-418

--- examples/gradle-migration-jvm/settings.gradle.kts
+++ examples/gradle-migration-jvm/settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
 
 plugins {
     // apply the plugin:
-    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-417")
+    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-418")
 }
 
 rootProject.name = "my-project-name"
