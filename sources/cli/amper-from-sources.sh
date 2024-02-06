79d268a147c56253b38eaaa7cd63859ccff39a3f
Leonid Shalupov <leonid@shalupov.com>
2024-02-06 14:48:35 +0100

    cli: fix amper-from-sources.sh on Linux

--- sources/cli/amper-from-sources.sh
+++ sources/cli/amper-from-sources.sh
@@ -11,10 +11,32 @@ set -e -u -o pipefail
 script_dir="$(dirname -- "$0")"
 script_dir="$(cd -- "$script_dir" && pwd)"
 
+case "$(uname)" in
+  # man stat on Mac OS X
+  # N       The name of the file.
+  # z       The size of file in bytes (st_size).
+  # a, m, c, B
+  #         The time file was last accessed or modified, or when the
+  #         inode was last changed, or the birth time of the inode
+  #         (st_atime, st_mtime, st_ctime, st_birthtime).
+  Darwin*) stat_format=(-f '%N %m %z') ;;
+
+  # man stat on Ubuntu
+  # %n     file name
+  # %s     total size, in bytes
+  # %Y     time of last data modification, seconds since Epoch
+  Linux*) stat_format=(--format='%n %s %Y') ;;
+
+  *)
+    echo "Unsupported system: $(uname)"
+    exit 1
+    ;;
+esac
+
 # do not call gradlew if nothing changed
 current_state() {
   find "$script_dir" '(' -name build -o -name .git -o -name .idea ')' -prune -o -type f -print0 | \
-   xargs -0 stat -f '%N %m %z'
+   xargs -0 stat "${stat_format[@]}"
 }
 
 up_to_date_file="$script_dir/build/up-to-date-from-sources-wrapper.txt"
