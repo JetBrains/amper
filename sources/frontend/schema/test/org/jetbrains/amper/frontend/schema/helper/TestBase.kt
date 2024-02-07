9213bd731f2a654f4011b40c2f059ba148e0003f
Leonid Shalupov <leonid@shalupov.com>
2024-02-07 20:56:18 +0100

    frontend: tests: create temp files on the same drive as test data
    some tests make relative paths and this fails on Windows otherwise

--- sources/frontend/schema/test/org/jetbrains/amper/frontend/schema/helper/TestBase.kt
+++ sources/frontend/schema/test/org/jetbrains/amper/frontend/schema/helper/TestBase.kt
@@ -4,16 +4,31 @@
 
 package org.jetbrains.amper.frontend.old.helper
 
-import org.junit.jupiter.api.io.TempDir
+import org.jetbrains.amper.test.TestUtil
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.Path
+import kotlin.io.path.deleteRecursively
 import kotlin.io.path.div
 
 abstract class TestBase(
     val baseTestResourcesPath: Path = Path("testResources")
 ) {
-    @TempDir
     lateinit var buildDir: Path
+
+    @BeforeEach
+    fun beforeEach() {
+        buildDir = Files.createTempDirectory(TestUtil.tempDir, "test-base")
+    }
+
+    @OptIn(ExperimentalPathApi::class)
+    @AfterEach
+    fun afterEach() {
+        buildDir.deleteRecursively()
+    }
 
     val buildFile: Path get() = buildDir / "build.yaml"
 }
