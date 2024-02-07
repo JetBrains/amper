8974036e1952554443c0355944cd80af861614e1
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2024-02-07 18:16:13 +0100

    [AMPER-237] exclude android-projects from amper project

---
+++ android-projects/simple-app/settings.gradle.kts
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+        maven("https://packages.jetbrains.team/maven/p/amper/amper")
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+    }
+}
+
+plugins {
+    id("org.jetbrains.amper.settings.plugin").version("0.2.0-dev-419")
+}
