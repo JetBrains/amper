86f0a4c03bc897a3c44d1c9e722bd9c4b2395e8c
Space Automation <no-reply@automation.jetbrains.space>
2024-02-12 18:39:21 +0000

    Update Amper plugin version to 0.3.0-dev-436

--- android-projects/simple-app/settings.gradle.kts
+++ android-projects/simple-app/settings.gradle.kts
@@ -14,5 +14,5 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.3.0-dev-435")
+    id("org.jetbrains.amper.settings.plugin").version("0.3.0-dev-436")
 }
