b5e5c5c71f7a29f2aa111867cff9661e0380b8ba
Leonid Shalupov <leonid@shalupov.com>
2024-02-12 13:10:35 +0100

    remove special treatment of Amper gradle plugin and convert them to libraries
    required change to bootstrap Amper project on Amper

--- sources/e2e-test/build.gradle.kts
+++ sources/e2e-test/build.gradle.kts
@@ -2,23 +2,12 @@
  * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-amper.useAmperLayout = true
-
-kotlin {
-    sourceSets {
-        val commonTest by sourceSets.getting {
-            dependencies {
-                implementation(dependencies.gradleTestKit())
-            }
-        }
-    }
-}
-
 tasks.withType<Test> {
     systemProperties["junit.jupiter.execution.parallel.enabled"] = true
     systemProperties["junit.jupiter.execution.parallel.mode.default"] = "concurrent"
 
     dependsOn(":sources:gradle-integration:jar")
+
     useJUnitPlatform()
     val inBootstrapMode: String? by project
     inBootstrapMode?.let {
@@ -29,13 +18,4 @@ tasks.withType<Test> {
         }
     }
     maxHeapSize = "4g"
-}
-
-private val pluginClasspathDir = "pluginUnderTestMetadata"
-
-tasks.findByName("jvmTestProcessResources")?.apply {
-    this as AbstractCopyTask
-    dependsOn(":sources:gradle-integration:copyDescriptorsHack")
-    dependsOn(":sources:gradle-integration:pluginUnderTestMetadata")
-    from(project(":sources:gradle-integration").buildDir.resolve(pluginClasspathDir))
 }
