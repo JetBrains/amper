770526c8b6d82280b1220d26c4bc70b9afbd8314
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2024-02-21 11:28:48 +0100

    [dr] add link to metadata specification

--- sources/dependency-resolution/src/org/jetbrains/amper/dependency/resolution/metadata/json/json.kt
+++ sources/dependency-resolution/src/org/jetbrains/amper/dependency/resolution/metadata/json/json.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 @file:Suppress("PropertyName")
@@ -15,6 +15,9 @@ private val json = Json {
     isLenient = true
 }
 
+/**
+ * [Gradle Module Metadata specification](https://github.com/gradle/gradle/blob/master/platforms/documentation/docs/src/docs/design/gradle-module-metadata-latest-specification.md)
+ */
 internal fun String.parseMetadata(): Module = json.decodeFromString(this)
 
 internal fun Module.serialize(): String = json.encodeToString(this)
