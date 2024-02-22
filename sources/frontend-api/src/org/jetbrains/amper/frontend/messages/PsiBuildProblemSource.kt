24f1a29de43efd7fbfd24505e5f2bfd4428016fd
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2024-02-22 15:54:09 +0100

    [parser] Split responsibilities of PsiBuildProblemSource
    This class tried to be two different things at the same time: for the file system elements and for the nodes of the file PSI. Decouple those instead into two subclasses of the sealed hierarchy.

--- sources/frontend-api/src/org/jetbrains/amper/frontend/messages/PsiBuildProblemSource.kt
+++ sources/frontend-api/src/org/jetbrains/amper/frontend/messages/PsiBuildProblemSource.kt
@@ -13,26 +13,47 @@ import org.jetbrains.amper.core.messages.LineAndColumnRange
 import org.jetbrains.amper.frontend.getLineAndColumnRangeInPsiFile
 import java.nio.file.Path
 
-class PsiBuildProblemSource(@UsedInIdePlugin val psiElement: PsiElement) : FileLocatedBuildProblemSource {
-    override val file: Path
-        get() = if (psiElement is PsiFileSystemItem) {
-            psiElement.virtualFile
-        } else {
-            psiElement.containingFile?.originalFile?.virtualFile
-        }?.toNioPathOrNull() ?: error("PSI element doesn't have real backing file")
+private const val NO_VIRTUAL_FILE_ERROR = "PSI element doesn't have real backing file"
 
-    override val range: LineAndColumnRange?
-        get() = if (psiElement is PsiFileSystemItem) {
-            null
-        } else {
-            getLineAndColumnRangeInPsiFile(psiElement)
-        }
+/**
+ * Location in the file designated by the node of a PSI tree.
+ * Is useful for IntelliJ integration as most of its features are working over PSI, so instead of determining
+ * the element at the offset, the IDE can just retrieve it as a field from the source.
+ *
+ * Use [PsiBuildProblemSource] factory function to construct.
+ */
+sealed interface PsiBuildProblemSource : FileLocatedBuildProblemSource {
+    @UsedInIdePlugin
+    val psiElement: PsiElement
 
-    override val offsetRange: IntRange?
-        get() = if (psiElement is PsiFileSystemItem) {
-            null
-        } else {
-            val range = psiElement.textRange
-            IntRange(range.startOffset, range.endOffset)
-        }
+    class FileSystemLike internal constructor(override val psiElement: PsiFileSystemItem) : PsiBuildProblemSource {
+        override val file: Path
+            get() = psiElement.virtualFile?.toNioPathOrNull() ?: error(NO_VIRTUAL_FILE_ERROR)
+
+        override val range: LineAndColumnRange? = null
+
+        override val offsetRange: IntRange? = null
+    }
+
+    class Element internal constructor(override val psiElement: PsiElement) : PsiBuildProblemSource {
+        override val file: Path
+            get() = psiElement.containingFile?.originalFile?.virtualFile?.toNioPathOrNull()
+                ?: error(NO_VIRTUAL_FILE_ERROR)
+
+        override val range: LineAndColumnRange
+            get() = getLineAndColumnRangeInPsiFile(psiElement)
+
+        override val offsetRange: IntRange
+            get() {
+                val range = psiElement.textRange
+                return IntRange(range.startOffset, range.endOffset)
+            }
+    }
 }
+
+fun PsiBuildProblemSource(psiElement: PsiElement): PsiBuildProblemSource =
+    if (psiElement is PsiFileSystemItem) {
+        PsiBuildProblemSource.FileSystemLike(psiElement)
+    } else {
+        PsiBuildProblemSource.Element(psiElement)
+    }
