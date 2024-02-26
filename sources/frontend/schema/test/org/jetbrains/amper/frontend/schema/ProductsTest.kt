34d42a55add8ed3684366e14cf87e9ed86c7251a
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2024-02-26 14:21:23 +0100

    [schema] Extract product diagnostics test into a dedicated test case

--- sources/frontend/schema/test/org/jetbrains/amper/frontend/schema/ProductsTest.kt
+++ sources/frontend/schema/test/org/jetbrains/amper/frontend/schema/ProductsTest.kt
@@ -1,41 +1,19 @@
 /*
- * Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.amper.frontend.schema
 
 import org.jetbrains.amper.frontend.old.helper.TestBase
 import org.jetbrains.amper.frontend.schema.helper.aomTest
-import org.jetbrains.amper.frontend.schema.helper.diagnosticsTest
 import kotlin.io.path.Path
 import kotlin.io.path.div
 import kotlin.test.Test
 
 internal class ProductsTest : TestBase(Path("testResources") / "product") {
-
-    @Test
-    fun `product with unsupported type `() {
-        diagnosticsTest("product-with-unsupported-type")
-    }
-
     @Test
     fun `product lib `() {
         aomTest("product-lib")
-    }
-
-    @Test
-    fun `product lib without platforms full`() {
-        diagnosticsTest("product-lib-without-platforms-full")
-    }
-
-    @Test
-    fun `product lib without platforms inline`() {
-        diagnosticsTest("product-lib-without-platforms-inline")
-    }
-
-    @Test
-    fun `product lib with empty platforms inline`() {
-        diagnosticsTest("product-lib-with-empty-platforms")
     }
 
     @Test
@@ -62,20 +40,4 @@ internal class ProductsTest : TestBase(Path("testResources") / "product") {
     fun `product app with non default platforms`() {
         aomTest("product-app-with-non-default-platforms")
     }
-
-    @Test
-    fun `product with incompatible platforms`() {
-        diagnosticsTest("product-with-incompatible-platforms")
-    }
-
-    @Test
-    fun `product with single unsupported platform`() {
-        diagnosticsTest("product-with-single-unsupported-platform")
-    }
-
-    @Test
-    fun `product with multiple unsupported platforms`() {
-        diagnosticsTest("product-with-multiple-unsupported-platforms")
-    }
-
 }
---
+++ sources/frontend/schema/test/org/jetbrains/amper/frontend/schema/diagnostics/ProductDiagnosticsTest.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.jetbrains.amper.frontend.schema.diagnostics
+
+import org.jetbrains.amper.frontend.old.helper.TestBase
+import org.jetbrains.amper.frontend.schema.helper.diagnosticsTest
+import kotlin.io.path.Path
+import kotlin.io.path.div
+import kotlin.test.Test
+
+class ProductDiagnosticsTest : TestBase(Path("testResources") / "diagnostics" / "product") {
+    @Test
+    fun `product with unsupported type `() {
+        diagnosticsTest("product-with-unsupported-type")
+    }
+
+    @Test
+    fun `product lib without platforms full`() {
+        diagnosticsTest("product-lib-without-platforms-full")
+    }
+
+    @Test
+    fun `product lib without platforms inline`() {
+        diagnosticsTest("product-lib-without-platforms-inline")
+    }
+
+    @Test
+    fun `product lib with empty platforms inline`() {
+        diagnosticsTest("product-lib-with-empty-platforms")
+    }
+
+    @Test
+    fun `product with incompatible platforms`() {
+        diagnosticsTest("product-with-incompatible-platforms")
+    }
+
+    @Test
+    fun `product with single unsupported platform`() {
+        diagnosticsTest("product-with-single-unsupported-platform")
+    }
+
+    @Test
+    fun `product with multiple unsupported platforms`() {
+        diagnosticsTest("product-with-multiple-unsupported-platforms")
+    }
+}
