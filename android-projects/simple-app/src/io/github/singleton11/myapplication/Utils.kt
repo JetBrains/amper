8bce0d1a4cad26c1fc8c929af6826f86c2c908ce
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-02-28 19:23:11 +0100

    AMPER-366 simple-app: test depending on main module

---
+++ android-projects/simple-app/src/io/github/singleton11/myapplication/Utils.kt
@@ -0,0 +1,5 @@
+package io.github.singleton11.myapplication
+
+object Utils {
+    fun sum(a: Int, b: Int) = a + b
+}
--- android-projects/simple-app/test/UnitTest.kt
+++ android-projects/simple-app/test/UnitTest.kt
@@ -2,13 +2,14 @@
  * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
+import io.github.singleton11.myapplication.Utils
 import kotlin.test.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 class UnitTest {
 
     @Test
     fun test() {
-        assertTrue { true }
+        assertEquals(4, Utils.sum(2, 2))
     }
 }
