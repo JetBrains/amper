65a27354aeb98d97f6a6f5ebc4c86ce0fdb90528
Vladimir.Naumenko <vladimir.naumenko@jetbrains.com>
2024-02-29 18:36:12 +0100

    restore-android-project

---
+++ sources/e2e-test/testData/projects/compose-android-ui/src/main/java/com/jetbrains/compose_android_ui_test/android/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.jetbrains.compose_android_ui_test.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hello android world")
+        }
+    }
+}
