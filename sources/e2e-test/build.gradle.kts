6440261dfecd0fc31bba0d43aa5e6a7a23528f31
Vladimir.Naumenko <vladimir.naumenko@jetbrains.com>
2024-02-29 16:35:16 +0100

    android tests task hotfix

--- sources/e2e-test/build.gradle.kts
+++ sources/e2e-test/build.gradle.kts
@@ -2,37 +2,41 @@
  * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-tasks.withType<Test> {
-    systemProperties["junit.jupiter.execution.parallel.enabled"] = true
-    systemProperties["junit.jupiter.execution.parallel.mode.default"] = "concurrent"
+tasks.withType<Test>().configureEach {
+    if (name != "runEmulatorTests") {
+        systemProperties["junit.jupiter.execution.parallel.enabled"] = true
+        systemProperties["junit.jupiter.execution.parallel.mode.default"] = "concurrent"
 
-    for (task in rootProject.getTasksByName("publishToMavenLocal", true)) {
-        dependsOn(task)
-    }
+        for (task in rootProject.getTasksByName("publishToMavenLocal", true)) {
+            dependsOn(task)
+        }
 
-    useJUnitPlatform()
-    val inBootstrapMode: String? by project
-    inBootstrapMode?.let {
-        if (it == "true") {
-            filter {
-                excludeTestsMatching("*BootstrapTest*")
+        useJUnitPlatform()
+
+        val inBootstrapMode: String? by project
+        inBootstrapMode?.let {
+            if (it == "true") {
+                filter {
+                    excludeTestsMatching("*BootstrapTest*")
+                }
             }
         }
-    }
-    maxHeapSize = "4g"
+        maxHeapSize = "4g"
 
-    exclude { include ->
-        include.file.name.contains("EmulatorTests")
+        exclude { include ->
+            include.file.name.contains("EmulatorTests")
+        }
     }
 }
 
-tasks.register("runEmulatorTests", Test::class) {
+val runEmulatorTests by tasks.registering(Test::class) {
     group = "verification"
     description = "Runs special tests from EmulatorTests.kt"
-    useJUnitPlatform()
 
-    filter {
-        includeTestsMatching(".*EmulatorTests.*")
+    useJUnitPlatform()
+    include { include ->
+        include.file.name.contains("EmulatorTests")
     }
     maxHeapSize = "4g"
 }
+
