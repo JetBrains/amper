5a6771e156ff8e71be61867756d78742944f2b39
Vladimir.Naumenko <vladimir.naumenko@jetbrains.com>
2024-03-14 21:23:50 +0100

    Add remote session and adb management to build script

--- sources/e2e-test/build.gradle.kts
+++ sources/e2e-test/build.gradle.kts
@@ -1,6 +1,11 @@
 /*
  * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
+import java.io.ByteArrayOutputStream
+
+
+val sessionInfoPath by extra { "$buildDir/device.session.json" }
+
 
 tasks.withType<Test>().configureEach {
     if (name != "runEmulatorTests") {
@@ -40,3 +45,105 @@ val runEmulatorTests by tasks.registering(Test::class) {
     maxHeapSize = "4g"
 }
 
+fun getOrCreateAdbRemoteSession(): String {
+    val stdout = ByteArrayOutputStream()
+    var adbCompanion = ""
+    project.exec {
+        commandLine = listOf("./scripts/espressoSession.sh", "-s", sessionInfoPath, "-n", "Amper UI Tests", "create")
+        standardOutput = stdout
+    }
+    println(stdout)
+    stdout.toString().lines().forEach {
+        println(it)
+        if (it.startsWith("ADB_COMPANION")) {
+            adbCompanion = it.split('=')[1]
+        }
+    }
+    return adbCompanion
+}
+
+fun createAdbRemoteSession() {
+    val stdout = ByteArrayOutputStream()
+    project.exec {
+        commandLine = listOf(
+            "./scripts/espressoSession.sh",
+            "-s",
+            sessionInfoPath,
+            "-n",
+            "Amper UI Tests",
+            "create"
+        )
+        standardOutput = stdout
+    }
+    println(stdout)
+}
+
+fun getAdbRemoteSession(): String {
+    val stdout = ByteArrayOutputStream()
+    project.exec {
+        commandLine = listOf(
+            "./scripts/espressoSession.sh",
+            "-s",
+            sessionInfoPath,
+            "port"
+        )
+        standardOutput = stdout
+    }
+    println(stdout)
+    val output = stdout.toString().trim()
+    if (output.isEmpty()) {
+        throw GradleException("Session wasn't created!")
+    }
+    return output
+}
+
+fun adb(vararg params: String): ByteArrayOutputStream {
+    val stdout = ByteArrayOutputStream()
+    val adbCompanion = getAdbRemoteSession()
+    val host = adbCompanion.split(':')
+    val cmd = mutableListOf<String>().apply {
+        add("${System.getenv("ANDROID_HOME")}/platform-tools/adb")
+        add("-H")
+        add(host[0])
+        add("-P")
+        add(host[1])
+        // add("-s", "emulator-5560") // Uncomment this line if needed
+        addAll(params)
+    }
+    println("Executing adb: $cmd")
+    project.exec {
+        commandLine = cmd
+        standardOutput = stdout
+    }
+    val cmdOutput = stdout.toString()
+    print(cmdOutput)
+    return stdout
+}
+
+tasks.register("createAndroidRemoteSession") {
+    doLast { createAdbRemoteSession() }
+}
+
+tasks.register<Exec>("deleteAndroidRemoteSession") {
+    commandLine = listOf("./scripts/espressoSession.sh", "-s", sessionInfoPath, "delete")
+}
+
+tasks.register("getEmulators") {
+    doLast {
+        val t = adb("devices")
+            .toString().lines().filter { it.contains("emulator") }
+            .forEach { println(it.trim().split("\\s+").first()) }
+    }
+}
+
+tasks.register("uninstallPackages") {
+    dependsOn("createAndroidRemoteSession")
+    doLast {
+        adb("shell", "pm", "list", "packages", "-3")
+            .toString().lines().filter { it.contains("jetbrains") && !it.contains("mocksharing") }
+            .forEach {
+                adb("uninstall", it.replace("package:", "")).toString()
+            }
+    }
+    mustRunAfter("createAndroidRemoteSession")
+}
---
+++ sources/e2e-test/scripts/espressoSession.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+WD_HUB_URL=${WD_HUB_URL:-"http://circlet:circlet@10.21.0.104:4444/wd/hub"}
+SESSION_URL="$WD_HUB_URL/session"
+SESSION_NAME="Android espresso tests"
+ANDROID_VERSION=${ANDROID_VERSION:-"8.1"}
+CAPABILITIES='{"desiredCapabilities": {"version": "__ANDROID_VERSION__", "platformName": "Android", "deviceName": "android", "name": "__SESSION_NAME__", "newCommandTimeout": 0, "sessionTimeout": "80m", "enableVNC": true, "portBindings": { "5037": "50001-50010" }}}'
+SESSION_INFO_FILE=".device.session.json"
+
+function get_session_id() {
+    if [ -f "$SESSION_INFO_FILE" ]; then
+        jq -r ".sessionId" "$SESSION_INFO_FILE"
+    fi
+}
+
+function print_session_info() {
+    SESSION_ID="$(get_session_id)"
+    ADB_COMPANION_HOST_PORT=$(get_adb_host)
+
+    [ -n "$SESSION_ID" ] && echo -e "DEVICE_SESSION_ID=$SESSION_ID\nADB_COMPANION=$ADB_COMPANION_HOST_PORT"
+}
+
+function check_session() {
+    SESSION_ID=$(get_session_id)
+    if [ -n "$SESSION_ID" ]; then
+        if curl -fsL -o /dev/null "$SESSION_URL/$SESSION_ID"; then
+            print_session_info
+        else
+            echo "Session not found" 1>&2
+        fi
+    fi
+}
+
+function create_session() {
+    if [ -n "$(check_session 2>/dev/null)" ]; then
+        print_session_info
+        return 1
+    fi
+
+    [ -f "$SESSION_INFO_FILE" ] && rm "$SESSION_INFO_FILE"
+
+    CAPABILITIES=$(echo "$CAPABILITIES" | sed "s|__SESSION_NAME__|$SESSION_NAME|")
+    CAPABILITIES=$(echo "$CAPABILITIES" | sed "s|__ANDROID_VERSION__|$ANDROID_VERSION|")
+    echo "Creating session with capabilities: $CAPABILITIES"
+
+    curl -fsSL --retry 30 --retry-max-time 300 -H "Content-type: application/json" -d "$CAPABILITIES" --create-dirs -o "$SESSION_INFO_FILE" "$SESSION_URL"
+    print_session_info
+}
+
+function get_adb_host() {
+    if [ -f "$SESSION_INFO_FILE" ]; then
+        ADB_COMPANION=$(jq -r '.value.portBindings."5037"' "$SESSION_INFO_FILE")
+        echo "$ADB_COMPANION"
+    fi
+}
+
+function delete_session() {
+    if [ -n "$(check_session)" ]; then
+        SESSION_ID=$(get_session_id)
+
+        echo "Deleting session: $SESSION_ID"
+        curl -fsSL -X DELETE -o /dev/null "$SESSION_URL/$SESSION_ID"
+        echo "Session deleted: $SESSION_ID"
+        rm "$SESSION_INFO_FILE"
+    fi
+}
+
+function usage() {
+cat <<END
+
+$(basename $0) [-s <session_info_file>] <action>
+
+Available actions:
+      create      Create new device session
+      check       Check if current session is still alive
+      port        Get adb host and mapped port
+      delete      Delete active session
+
+Options:
+      -s path     Use specified file path to store/read information about device session
+
+END
+}
+
+
+while getopts ":hs:n:v:" opt; do
+    case ${opt} in
+        s)
+            SESSION_INFO_FILE=$OPTARG
+            ;;
+        n)
+            SESSION_NAME=$OPTARG
+            ;;
+	    v)
+            ANDROID_VERSION=$OPTARG
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        \?)
+            usage
+            exit 1
+            ;;
+        :)
+            echo "Invalid option: '$OPTARG' requires an argument" 1>&2
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+
+case "$1" in
+    create)
+        create_session
+        ;;
+    check)
+        check_session
+        ;;
+    port)
+        get_adb_host
+        ;;
+    delete)
+        delete_session
+        ;;
+    \?)
+        echo "Unknown operation" 1>&2
+        usage
+        exit 1
+esac
+
+exit 0
+
