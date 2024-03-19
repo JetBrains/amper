a3e1acca01036dfdd9c6cba0a920a51823de52cd
Vladimir.Naumenko <vladimir.naumenko@jetbrains.com>
2024-03-19 03:42:16 +0100

    fix includeBuild

--- sources/e2e-test/testData/projects/compose-android-ui/settings.gradle.kts
+++ sources/e2e-test/testData/projects/compose-android-ui/settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         google()
         gradlePluginPortal()
     }
-    includeBuild("../../../../..") // <REMOVE_LINE_IF_RUN_WITH_PLUGIN_CLASSPATH>
+    includeBuild(".../../../../build")
 }
 
 plugins {
