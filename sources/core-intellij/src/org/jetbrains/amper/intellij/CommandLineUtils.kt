cf9dc8fdacd4f5331bc9bf91cc54e89b2a1e3633
Alexander.Tsarev <alexander.tsarev@jetbrains.com>
2024-03-25 16:17:42 +0100

    [cli/ios] Fix binary compatibility issues. Update intellij version.

--- sources/core-intellij/src/org/jetbrains/amper/intellij/CommandLineUtils.kt
+++ sources/core-intellij/src/org/jetbrains/amper/intellij/CommandLineUtils.kt
@@ -5,8 +5,40 @@
 package org.jetbrains.amper.intellij
 
 import com.intellij.execution.CommandLineUtil
+import com.intellij.execution.Platform
 
 object CommandLineUtils {
     fun quoteCommandLineForCurrentPlatform(cmd: List<String>): List<String> =
-        CommandLineUtil.toCommandLine(cmd)
+        CommandLineUtil.toCommandLine(cmd[0], cmd.subList(1, cmd.size), SystemInfoRt.currentPlatform())
 }
+
+// Copy-paste from IntelliJ [SystemInfoRt] due to a small binary incompatibility.
+object SystemInfoRt {
+    val OS_NAME: String
+    val OS_VERSION: String
+
+    init {
+        var name = System.getProperty("os.name")
+        var version = System.getProperty("os.version").lowercase()
+
+        if (name.startsWith("Windows") && name.matches("Windows \\d+".toRegex())) {
+            // for whatever reason, JRE reports "Windows 11" as a name and "10.0" as a version on Windows 11
+            try {
+                val version2 = name.substring("Windows".length + 1) + ".0"
+                if (version2.toFloat() > version.toFloat()) {
+                    version = version2
+                }
+            } catch (ignored: NumberFormatException) {
+            }
+            name = "Windows"
+        }
+
+        OS_NAME = name
+        OS_VERSION = version
+    }
+
+    private val _OS_NAME = OS_NAME.lowercase()
+    val isWindows: Boolean = _OS_NAME.startsWith("windows")
+    fun currentPlatform()= if (isWindows) Platform.WINDOWS else Platform.UNIX
+}
+
