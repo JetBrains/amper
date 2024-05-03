9eba15d469c946f3e6b0b1c880362c642540f0b3
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2024-05-03 11:13:45 +0200

    Remove accidentally added file

--- migrated-projects/compose-multiplatform-template/shared/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-tasks.withType<Copy> {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
-
-tasks.filter { it.name.contains("compose") }.filterIsInstance<Copy>().forEach {
-    it.configure<Copy> {
-
-    }
-}
