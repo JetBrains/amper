43908829033b889d2416663acd6263b1bc7b330a
Anton Makeev <Anton.Makeev@jetbrains.com>
2024-05-07 14:55:46 +0200

    remove copyright notice from examples and templates

--- examples-gradle/coverage/test/CoverageTest.kt
+++ examples-gradle/coverage/test/CoverageTest.kt
@@ -1,7 +1,3 @@
-/*
- * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
-
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
--- examples-gradle/gradle-migration-kmp/android-app/build.gradle.kts
+++ examples-gradle/gradle-migration-kmp/android-app/build.gradle.kts
@@ -1,7 +1,3 @@
-/*
- * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
-
 plugins {
     // Apply any plugins as usual.
     // Note: The following plugins are preconfigured and their versions can't be changed:
