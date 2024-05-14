2908632c3298812e28135ba92ca5a5137888316b
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2024-05-14 19:29:51 +0200

    [test] align plugin versions in migrated projects

--- migrated-projects/compose-multiplatform-ios-android-template/settings.gradle.kts
+++ migrated-projects/compose-multiplatform-ios-android-template/settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
 
 plugins {
     // apply the plugin:
-    id("org.jetbrains.amper.settings.plugin").version("0.3.0-dev-563")
+    id("org.jetbrains.amper.settings.plugin").version("0.3.0-dev-571")
 }
 
 rootProject.name = "compose-multiplatform-ios-android-template"
