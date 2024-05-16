26bd3bd73e15489483c6ac03e373c1aaa8bde019
Joffrey.Bion <joffrey.bion@jetbrains.com>
2024-05-16 12:34:21 +0200

    [backend] Add compose compiler versions and cleanup the docs about versioning beyond 2.0.0-RC2

--- sources/cli/src/org/jetbrains/amper/compilation/KotlinCompilerDownloader.kt
+++ sources/cli/src/org/jetbrains/amper/compilation/KotlinCompilerDownloader.kt
@@ -6,7 +6,6 @@ package org.jetbrains.amper.compilation
 
 import com.sun.jna.Platform
 import org.jetbrains.amper.cli.AmperUserCacheRoot
-import org.jetbrains.amper.core.UsedVersions
 import org.jetbrains.amper.core.extract.ExtractOptions
 import org.jetbrains.amper.dependency.resolution.ResolutionScope
 import org.jetbrains.amper.downloader.Downloader
@@ -66,28 +65,29 @@ class KotlinCompilerDownloader(
     /**
      * Downloads the Kotlin Compose compiler plugin for the given [kotlinVersion] (NOT the compose library version).
      *
-     * The [kotlinVersion] should match the Kotlin version requested by the user, the plugin will be added to
-     * the Kotlin compiler command line of version [kotlinVersion].
+     * The [kotlinVersion] should be the version of the Kotlin compiler itself (not Compose). For Kotlin versions
+     * before 2.0.0-RC2, the legacy Compose compiler artifact will be downloaded in the correct version based on a
+     * "well-known" mapping. For newer versions, the new Compose compiler artifact will be downloaded with the same
+     * version as [kotlinVersion].
      */
     suspend fun downloadKotlinComposePlugin(kotlinVersion: String): Path {
-        UsedVersions.composeCompilerVersionFor(kotlinVersion)?.let {  composeCompilerVersionFor ->
-            return downloadKotlinCompilerPlugin(
+        val legacyComposeVersion = legacyComposeCompilerVersionFor(kotlinVersion)
+        return if (legacyComposeVersion != null) {
+            downloadKotlinCompilerPlugin(
                 groupId = "org.jetbrains.compose.compiler",
                 artifactId = "compiler",
-                // This error should be gracefully reported in the frontend, so it's OK to crash here
-                version = composeCompilerVersionFor
+                version = legacyComposeVersion,
             )
-        } ?: run {
-            return downloadKotlinCompilerPlugin(
-                groupId = "org.jetbrains.kotlin",
-                artifactId = "kotlin-compose-compiler-plugin-embeddable",
-                // This error should be gracefully reported in the frontend, so it's OK to crash here
-                version = kotlinVersion
+        } else {
+            downloadKotlinCompilerPlugin(
+                groupId = KOTLIN_GROUP_ID,
+                artifactId = "kotlin-compose-compiler-plugin-embeddable", // new artifact since 2.0.0-RC2
+                version = kotlinVersion, // the new artifact uses a version matching the Kotlin version
             )
         }
     }
 
-    private suspend fun downloadKotlinCompilerPlugin(groupId: String = KOTLIN_GROUP_ID, artifactId: String, version: String): Path {
+    private suspend fun downloadKotlinCompilerPlugin(groupId: String, artifactId: String, version: String): Path {
         val artifacts = downloadMavenArtifact(
             groupId = groupId,
             artifactId = artifactId,
@@ -175,4 +175,49 @@ class KotlinCompilerDownloader(
         val downloadedArchive = Downloader.downloadFileToCacheLocation(artifactUri.toString(), userCacheRoot)
         return extractFileToCacheLocation(downloadedArchive, userCacheRoot, *extractOptions)
     }
+}
+
+/**
+ * Returns the legacy Compose (multiplatform) compiler plugin version matching the given [kotlinVersion], or null if
+ * the new Compose compiler artifact with the same version as Kotlin should be used (i.e. Kotlin >= 2.0.0-RC2).
+ *
+ * **IMPORTANT:** The user-defined Compose version is only for runtime libraries and the Compose Gradle plugin.
+ * The Compose compiler has a different versioning scheme, with a mapping to the Kotlin compiler versions.
+ *
+ * ### Implementation note
+ *
+ * This mapping should not have to be updated anymore, because the Compose compiler is now part of the Kotlin repository
+ * and is released with the same version as Kotlin itself.
+ *
+ * The original mapping in this function came from the tables in
+ * [the official documentation](https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-compatibility-and-versioning.html),
+ * and the
+ * [mapping in the Gradle plugin](https://github.com/JetBrains/compose-multiplatform/blob/master/gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatibility.kt).
+ */
+private fun legacyComposeCompilerVersionFor(kotlinVersion: String): String? = when (kotlinVersion) {
+    "1.7.10" -> "1.3.0"
+    "1.7.20" -> "1.3.2.2"
+    "1.8.0" -> "1.4.0"
+    "1.8.10" -> "1.4.2"
+    "1.8.20" -> "1.4.5"
+    "1.8.21" -> "1.4.7"
+    "1.8.22" -> "1.4.8"
+    "1.9.0-Beta" -> "1.4.7.1-beta"
+    "1.9.0-RC" -> "1.4.8-beta"
+    "1.9.0" -> "1.5.1"
+    "1.9.10" -> "1.5.2"
+    "1.9.20-Beta" -> "1.5.2.1-Beta2"
+    "1.9.20-Beta2" -> "1.5.2.1-Beta3"
+    "1.9.20-RC" -> "1.5.2.1-rc01"
+    "1.9.20-RC2" -> "1.5.3-rc01"
+    "1.9.20" -> "1.5.3"
+    "1.9.21" -> "1.5.4"
+    "1.9.22" -> "1.5.8.1"
+    "1.9.23" -> "1.5.12.5"
+    "1.9.24" -> "1.5.14"
+    "2.0.0-Beta1" -> "1.5.4-dev1-kt2.0.0-Beta1"
+    "2.0.0-Beta4" -> "1.5.9-kt-2.0.0-Beta4"
+    "2.0.0-Beta5" -> "1.5.11-kt-2.0.0-Beta5"
+    "2.0.0-RC1" -> "1.5.11-kt-2.0.0-RC1"
+    else -> null // since 2.0.0-RC2, the Compose compiler version matches the Kotlin version (/!\ different artifact)
 }
--- sources/core/src/org/jetbrains/amper/core/UsedVersions.kt
+++ sources/core/src/org/jetbrains/amper/core/UsedVersions.kt
@@ -22,43 +22,4 @@ object UsedVersions {
 
     // No replacement yet.
     val junit5Version = "5.9.2"
-
-    /**
-     * Returns the Compose (multiplatform) compiler plugin version matching the given [kotlinVersion].
-     * 
-     * **IMPORTANT:** The user-defined Compose version is only for runtime libraries and the Compose Gradle plugin.
-     * The Compose compiler has a different versioning scheme, with a mapping to the Kotlin compiler versions.
-     *
-     * **How to update this list**
-     *
-     * The main table in the official documentation table lists the Compose Gradle plugin (and runtime library) version.
-     * We have to look at [the dev versions table](https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-compatibility-and-versioning.html#use-a-developer-version-of-compose-multiplatform-compiler)
-     * to get information about the Compose compiler plugin itself.
-     *
-     * Look at the [mapping in the Gradle plugin](https://github.com/JetBrains/compose-multiplatform/blob/master/gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatibility.kt)
-     * for the most up-to-date reference.
-     */
-    fun composeCompilerVersionFor(kotlinVersion: String): String? = when (kotlinVersion) {
-        "1.7.10" -> "1.3.0"
-        "1.7.20" -> "1.3.2.2"
-        "1.8.0" -> "1.4.0"
-        "1.8.10" -> "1.4.2"
-        "1.8.20" -> "1.4.5"
-        "1.8.21" -> "1.4.7"
-        "1.8.22" -> "1.4.8"
-        "1.9.0-Beta" -> "1.4.7.1-beta"
-        "1.9.0-RC" -> "1.4.8-beta"
-        "1.9.0" -> "1.5.1"
-        "1.9.10" -> "1.5.2"
-        "1.9.20-Beta" -> "1.5.2.1-Beta2"
-        "1.9.20-Beta2" -> "1.5.2.1-Beta3"
-        "1.9.20-RC" -> "1.5.2.1-rc01"
-        "1.9.20-RC2" -> "1.5.3-rc01"
-        "1.9.20" -> "1.5.3"
-        "1.9.21" -> "1.5.4"
-        "1.9.22" -> "1.5.8.1"
-        "2.0.0-Beta1" -> "1.5.4-dev1-kt2.0.0-Beta1"
-        "2.0.0-Beta4" -> "1.5.9-kt-2.0.0-Beta4"
-        else -> null // the error will be handled differently depending on the caller
-    }
 }
