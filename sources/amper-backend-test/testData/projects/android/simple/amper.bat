cfc23b0bc44892b598a8d14ed8d6a6379e6f8e39
Space Automation <no-reply@automation.jetbrains.space>
2024-05-17 09:44:07 +0000

    Update Amper plugin version to 0.4.0-dev-576

--- sources/amper-backend-test/testData/projects/android/simple/amper.bat
+++ sources/amper-backend-test/testData/projects/android/simple/amper.bat
@@ -10,11 +10,11 @@
 
 setlocal
 
-set amper_version=0.4.0-dev-575
+set amper_version=0.4.0-dev-576
 set amper_url=https://packages.jetbrains.team/maven/p/amper/amper/org/jetbrains/amper/cli/%amper_version%/cli-%amper_version%-dist.zip
 
 @rem Establish chain of trust from here by specifying exact checksum of Amper distribution to be run
-set amper_sha256=30d74878571b755c51f3bf9bbaa341a7f947bdbcfedf815e086514d9dcd03369
+set amper_sha256=4797b39a317017c33226b1df86c6f59436972900b5f6c03f888e0846849b84c7
 
 if "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
     set jvm_url=https://aka.ms/download-jdk/microsoft-jdk-17.0.6-windows-aarch64.zip
