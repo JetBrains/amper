574532280c59ddaf0c9c05b7f7f464bebe175a8d
Joffrey.Bion <joffrey.bion@jetbrains.com>
2024-06-11 18:23:40 +0200

    [gradle] Extract SLF4JProblemReporter from entryPoint.kt

--- sources/gradle-integration/src/org/jetbrains/amper/gradle/entryPoint.kt
+++ sources/gradle-integration/src/org/jetbrains/amper/gradle/entryPoint.kt
@@ -35,33 +35,3 @@ class BindingSettingsPlugin : Plugin<Settings> {
         }
     }
 }
-
-internal class SLF4JProblemReporterContext : ProblemReporterContext {
-    override val problemReporter: SLF4JProblemReporter = SLF4JProblemReporter(BindingSettingsPlugin::class.java)
-}
-
-internal class SLF4JProblemReporter(loggerClass: Class<*> = ProblemReporter::class.java) : CollectingProblemReporter() {
-    companion object {
-        private const val ERROR_PREFIX = "  - "
-        private const val ERROR_INDENT = "    "
-    }
-
-    private val logger = LoggerFactory.getLogger(loggerClass)
-
-    override fun doReportMessage(message: BuildProblem) {
-        when (message.level) {
-            Level.Warning -> logger.warn(renderMessage(message))
-            Level.Error -> logger.error(renderMessage(message))
-            Level.Fatal -> logger.error(renderMessage(message))
-        }
-    }
-
-    fun getErrors(): List<String> = problems.map(::renderMessage)
-
-    fun getGradleError(): String =
-        """
-        |Amper model initialization failed. 
-        |Errors: 
-        |$ERROR_PREFIX${getErrors().joinToString("\n|$ERROR_PREFIX") { it.replace("\n", "\n$ERROR_INDENT") }}
-        |See logs for details.""".trimMargin()
-}
