95b6ef48649b276f8fd3b888e4368af35ccde195
Joffrey.Bion <joffrey.bion@jetbrains.com>
2024-06-20 13:14:43 +0200

    [test] Use licensesToAccept instead of hardcoding the list twice

--- sources/test-base/src/org/jetbrains/amper/test/TestUtil.kt
+++ sources/test-base/src/org/jetbrains/amper/test/TestUtil.kt
@@ -147,26 +147,9 @@ object TestUtil {
 
                 extractZip(commandLineTools, root / "cmdline-tools", true)
 
-                acceptAndroidLicense(root, "android-googletv-license", "601085b94cd77f0b54ff86406957099ebe79c4d6")
-                acceptAndroidLicense(root, "android-sdk-license", "8933bad161af4178b1185d1a37fbf41ea5269c55")
-                acceptAndroidLicense(root, "android-sdk-license", "d56f5187479451eabf01fb78af6dfcb131a6481e")
-                acceptAndroidLicense(root, "android-sdk-license", "24333f8a63b6825ea9c5514f83c2829b004d1fee")
-                acceptAndroidLicense(
-                    root,
-                    "android-sdk-preview-license",
-                    "84831b9409646a918e30573bab4c9c91346d8abd"
-                )
-                acceptAndroidLicense(
-                    root,
-                    "android-sdk-preview-license",
-                    "504667f4c0de7af1a06de9f4b1727b84351f2910"
-                )
-                acceptAndroidLicense(root, "google-gdk-license", "33b6a2b64607f11b759f320ef9dff4ae5c47d97a")
-                acceptAndroidLicense(
-                    root,
-                    "intel-android-extra-license",
-                    "d975f751698a77b662f1254ddbeed3901e976f5a"
-                )
+                licensesToAccept.forEach { (name, hash) ->
+                    acceptAndroidLicense(root, name, hash)
+                }
 
                 val jdk = JdkDownloader.getJdk(fakeUserCacheRoot)
                 for (tool in toolsToInstall) {
