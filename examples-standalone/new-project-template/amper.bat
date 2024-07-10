77248928709290670b25a893396a699a11adeeac
Space Automation <no-reply@automation.jetbrains.space>
2024-07-10 19:49:09 +0000

    Update Amper plugin version to 0.4.0-dev-643

--- examples-standalone/new-project-template/amper.bat
+++ examples-standalone/new-project-template/amper.bat
@@ -26,11 +26,11 @@ if defined AMPER_JRE_DOWNLOAD_ROOT (
   set amper_jre_download_root_defined=https:/
 )
 
-set amper_version=0.4.0-dev-642
+set amper_version=0.4.0-dev-643
 set amper_url=%amper_download_root_defined%/org/jetbrains/amper/cli/%amper_version%/cli-%amper_version%-dist.zip
 
 @rem Establish chain of trust from here by specifying exact checksum of Amper distribution to be run
-set amper_sha256=af13dc06a00cf2b8a552c490f938dca549c38dc21306813ab6db7ff4070c89f0
+set amper_sha256=f9a0aa6bbdff865e6a5b2b61203f1b8d48c969397d73116ff9f4c4677a55681e
 
 if "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
     set jvm_url=%amper_jre_download_root_defined%/aka.ms/download-jdk/microsoft-jdk-17.0.6-windows-aarch64.zip
--- migrated-projects/KaMPKit/settings.gradle.kts
+++ migrated-projects/KaMPKit/settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.4.0-dev-642")
+    id("org.jetbrains.amper.settings.plugin").version("0.4.0-dev-643")
 }
 
 include(":app")
