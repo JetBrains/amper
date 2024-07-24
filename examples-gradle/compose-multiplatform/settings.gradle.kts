17987bc1fd1d2853ffcd5c126e5a0dfa5ebcbccd
Space Automation <no-reply@automation.jetbrains.space>
2024-07-24 20:30:24 +0000

    Update Amper plugin version to 0.4.0-dev-737

--- examples-gradle/compose-multiplatform/settings.gradle.kts
+++ examples-gradle/compose-multiplatform/settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.4.0-dev-733")
+    id("org.jetbrains.amper.settings.plugin").version("0.4.0-dev-737")
 }
 
 include(":android-app")
