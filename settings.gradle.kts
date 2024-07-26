4ef6e9279475ad7bfccf2e1c64f992d70e56bcd2
Space Automation <no-reply@automation.jetbrains.space>
2024-07-26 20:08:14 +0000

    Update Amper plugin version to 0.4.0-dev-772

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,7 +31,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.4.0-dev-740")
+    id("org.jetbrains.amper.settings.plugin").version("0.4.0-dev-772")
 }
 
 include(":sources:amper-backend-test")
