30ef3f96445d23ef28a704253a4c0b6f39a96d3b
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2024-09-05 11:32:23 +0200

    Don't let JGit access global gitconfig
    We call JGit to calculate the snapshot of the current local state of the code. While doing this, JGit reads all relevant git config files and fails if it recognizes an unsupported parameter (e.g. `diff.algorithm=patience`). Such a parameter can come from a global or system gitconfig, and the easiest way to workaround the issue is to avoid reading global/system gitconfigs (since it is unnecessary for the task anyway).

--- sources/core/build.gradle.kts
+++ sources/core/build.gradle.kts
@@ -3,6 +3,10 @@
  */
 
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.Config
+import org.eclipse.jgit.storage.file.FileBasedConfig
+import org.eclipse.jgit.util.FS
+import org.eclipse.jgit.util.SystemReader
 import java.security.MessageDigest
 
 buildscript {
@@ -24,20 +28,22 @@ val generateBuildProperties by tasks.creating(WriteProperties::class.java) {
     if (project.findProperty("inBootstrapMode") != "true") {
         val gitRoot = rootProject.projectDir.resolve(".git")
         if (gitRoot.exists()) {
-            val git = Git.open(gitRoot)
-            val repo = git.repository
-            val head = repo.getReflogReader("HEAD").lastEntry
-            val shortHash = repo.newObjectReader().use { it.abbreviate(head.newId).name() }
-            property("commitHash", head.newId.name)
-            property("commitShortHash", shortHash)
-            property("commitDate", head.who.`when`.toInstant())
+            dontReadGlobalGitConfig {
+                val git = Git.open(gitRoot)
+                val repo = git.repository
+                val head = repo.getReflogReader("HEAD").lastEntry
+                val shortHash = repo.newObjectReader().use { it.abbreviate(head.newId).name() }
+                property("commitHash", head.newId.name)
+                property("commitShortHash", shortHash)
+                property("commitDate", head.who.`when`.toInstant())
 
-            // When developing locally, we want to somehow capture changes to the local sources because we want to
-            // invalidate incremental state files based on this. Using the git index for this is insufficient because
-            // it only captures the paths but not the contents of the files.
-            // That's why we use a digest of the whole diff.
-            val localDiffHash = git.diff().call().map { it.newId.toObjectId().name }.hash()
-            property("localChangesHash", localDiffHash)
+                // When developing locally, we want to somehow capture changes to the local sources because we want to
+                // invalidate incremental state files based on this. Using the git index for this is insufficient because
+                // it only captures the paths but not the contents of the files.
+                // That's why we use a digest of the whole diff.
+                val localDiffHash = git.diff().call().map { it.newId.toObjectId().name }.hash()
+                property("localChangesHash", localDiffHash)
+            }
         }
     }
 }
@@ -53,4 +59,31 @@ private fun Iterable<String>.hash(): String {
 tasks.jvmProcessResources {
     dependsOn(generateBuildProperties)
     from(generateBuildProperties)
+}
+
+private fun dontReadGlobalGitConfig(work: () -> Unit) {
+    val oldSystemReader = SystemReader.getInstance()
+    try {
+        SystemReader.setInstance(EmptyConfigSystemReader(oldSystemReader))
+        work()
+    }
+    finally {
+        SystemReader.setInstance(oldSystemReader)
+    }
+}
+
+private class EmptyConfigSystemReader(private val delegate: SystemReader = getInstance()) : SystemReader() {
+    override fun getHostname(): String? = delegate.hostname
+    override fun getenv(variable: String?): String? = delegate.getenv(variable)
+    override fun getProperty(key: String?): String? = delegate.getProperty(key)
+    override fun getCurrentTime(): Long = delegate.currentTime
+    override fun getTimezone(`when`: Long): Int = delegate.getTimezone(`when`)
+    override fun openUserConfig(parent: Config?, fs: FS?): FileBasedConfig = NoopFileBasedConfig(parent, fs)
+    override fun openSystemConfig(parent: Config?, fs: FS?): FileBasedConfig = NoopFileBasedConfig(parent, fs)
+    override fun openJGitConfig(parent: Config?, fs: FS?): FileBasedConfig = delegate.openJGitConfig(parent, fs)
+}
+
+private class NoopFileBasedConfig(parent: Config?, fs: FS?) : FileBasedConfig(parent, null, fs) {
+    override fun load() {}
+    override fun isOutdated(): Boolean = false
 }
