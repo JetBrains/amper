51249770a0c1005b6e84b9e4162c981f0abc1822
Space Automation <no-reply@automation.jetbrains.space>
2024-10-03 13:56:55 +0000

    Update Amper version to 0.5.0-dev-1563

--- sources/amper-backend-test/testData/projects/android/simple/amper.bat
+++ sources/amper-backend-test/testData/projects/android/simple/amper.bat
@@ -26,11 +26,11 @@ if defined AMPER_JRE_DOWNLOAD_ROOT (
   set amper_jre_download_root_defined=https:/
 )
 
-set amper_version=0.5.0-dev-1554
+set amper_version=0.5.0-dev-1563
 set amper_url=%amper_download_root_defined%/org/jetbrains/amper/cli/%amper_version%/cli-%amper_version%-dist.zip
 
 @rem Establish chain of trust from here by specifying exact checksum of Amper distribution to be run
-set amper_sha256=c77269f4889838647cf183cf1d93b0878bde55e716b867465610632da6f69c04
+set amper_sha256=ae73210f6915c56e83982bc4de73f76764f1786fb96bf549eee238b2bcf6a3bc
 
 if "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
     set jvm_url=%amper_jre_download_root_defined%/aka.ms/download-jdk/microsoft-jdk-17.0.6-windows-aarch64.zip
