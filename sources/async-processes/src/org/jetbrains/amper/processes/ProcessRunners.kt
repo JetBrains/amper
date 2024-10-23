3bc8728576c94761ada8581bd66e012f81ecf6a8
Joffrey.Bion <joffrey.bion@jetbrains.com>
2024-10-23 02:46:05 +0200

    [infra] Add inspection to push using helpers instead of ProcessBuilder.start()

--- sources/async-processes/src/org/jetbrains/amper/processes/ProcessRunners.kt
+++ sources/async-processes/src/org/jetbrains/amper/processes/ProcessRunners.kt
@@ -2,6 +2,8 @@
  * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
+@file:Suppress("PROCESS_BUILDER_START_LEAK")
+
 package org.jetbrains.amper.processes
 
 import kotlinx.coroutines.Dispatchers
