cce3e097a046162e7767b8448322268cbb663494
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-01-10 19:20:16 +0100

    [test] AMPER-4008 Add Amper bootstrap test using Amper to compile Amper

---
+++ sources/amper-cli-test/test/org/jetbrains/amper/cli/test/AmperBootstrapTest.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.jetbrains.amper.cli.test
+
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.amper.test.TestUtil
+import java.nio.file.Path
+import kotlin.test.Test
+
+class AmperBootstrapTest : AmperCliTestBase() {
+
+    override val testDataRoot: Path
+        get() = error("testDataRoot shouldn't be used")
+
+    @Test
+    fun `amper can build itself`() {
+        runBlocking {
+            // We don't want to run the 'test' command because that would amount to run the whole test suite twice.
+            // Testing that the project compiles with the new sources is good enough, as the tests will be covered
+            // in the build of the MR that bumps Amper in Amper.
+            runCli(projectRoot = TestUtil.amperCheckoutRoot, "build")
+        }
+    }
+}
