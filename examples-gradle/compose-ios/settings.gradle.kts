974da9d10889ee302d2bf7e31b955bb8e254f85b
Space Automation <no-reply@automation.jetbrains.space>
2025-01-26 12:14:01 +0000

    Update Amper version to 0.6.0-dev-2419

--- examples-gradle/compose-ios/settings.gradle.kts
+++ examples-gradle/compose-ios/settings.gradle.kts
@@ -10,5 +10,5 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.6.0-dev-2417")
+    id("org.jetbrains.amper.settings.plugin").version("0.6.0-dev-2419")
 }
--- examples-standalone-amperlang/new-project-template/amper.bat
+++ examples-standalone-amperlang/new-project-template/amper.bat
@@ -16,9 +16,9 @@
 setlocal
 
 @rem The version of the Amper distribution to provision and use
-set amper_version=0.6.0-dev-2417
+set amper_version=0.6.0-dev-2419
 @rem Establish chain of trust from here by specifying exact checksum of Amper distribution to be run
-set amper_sha256=d50b8d90519717bced33b39dff180225daf18c2dccb152e7a58243036362d888
+set amper_sha256=6fd68097ce81f8f838d41deab07cab6c537b095c016e3202892fa8f5a20f23ce
 
 if not defined AMPER_DOWNLOAD_ROOT set AMPER_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined AMPER_JRE_DOWNLOAD_ROOT set AMPER_JRE_DOWNLOAD_ROOT=https:/
