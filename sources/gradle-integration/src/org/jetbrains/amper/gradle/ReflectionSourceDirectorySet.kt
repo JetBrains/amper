de49b5c9bac942cf2ce5c31cd674c26eec6b4e03
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2025-03-10 15:09:26 +0100

    [warnings] Fix warnings in ReflectionSourceDirectorySet
    - Proper constant naming
    - Suppress sourcesGetter cast
    - Override default Java methods (as they're not overridden by the delegation)

--- sources/gradle-integration/src/org/jetbrains/amper/gradle/ReflectionSourceDirectorySet.kt
+++ sources/gradle-integration/src/org/jetbrains/amper/gradle/ReflectionSourceDirectorySet.kt
@@ -1,25 +1,26 @@
 /*
- * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.amper.gradle
 
 import org.gradle.api.file.SourceDirectorySet
+import java.io.File
+import java.util.*
+import java.util.function.Consumer
 
 /**
- * Class to access private "sources" field, to be able to replace them without changing
+ * Class to access the private "sources" field, to be able to replace them without changing
  * (For example, do not changing provider).
  */
 class ReflectionSourceDirectorySet private constructor(
     private val delegate: SourceDirectorySet
 ) : SourceDirectorySet by delegate {
     companion object {
-
-        private const val sdsFqn = "org.gradle.api.internal.file.DefaultSourceDirectorySet"
+        private const val SOURCE_DIRECTORY_SET_FQN = "org.gradle.api.internal.file.DefaultSourceDirectorySet"
 
         private val defaultSDSClass by lazy {
-            this::class.java
-                .classLoader.loadClass(sdsFqn)
+            this::class.java.classLoader.loadClass(SOURCE_DIRECTORY_SET_FQN)
         }
 
         private val sourcesField by lazy {
@@ -28,12 +29,13 @@ class ReflectionSourceDirectorySet private constructor(
                 .apply { isAccessible = true }
         }
 
+        @Suppress("UNCHECKED_CAST")
         private val sourcesGetter: (Any) -> MutableList<Any> = {
             sourcesField.get(it) as MutableList<Any>
         }
 
         /**
-         * Try to wrap [DefaultSourceDirectorySet] with reflection adapter.
+         * Try to wrap [org.gradle.api.internal.file.DefaultSourceDirectorySet] with reflection adapter.
          */
         fun tryWrap(toWrap: SourceDirectorySet): ReflectionSourceDirectorySet? =
             if (defaultSDSClass.isInstance(toWrap))
@@ -46,4 +48,12 @@ class ReflectionSourceDirectorySet private constructor(
     }
 
     val mutableSources get() = sourcesGetter(delegate)
+
+    override fun forEach(action: Consumer<in File>?) {
+        delegate.forEach(action)
+    }
+
+    override fun spliterator(): Spliterator<File?> {
+        return delegate.spliterator()
+    }
 }
