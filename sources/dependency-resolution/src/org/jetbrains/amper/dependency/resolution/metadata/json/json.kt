3ffaebebec5d645e302dff361d2d96adac3c188e
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2025-03-17 19:00:18 +0100

    Remove redundant suppression

--- sources/dependency-resolution/src/org/jetbrains/amper/dependency/resolution/metadata/json/json.kt
+++ sources/dependency-resolution/src/org/jetbrains/amper/dependency/resolution/metadata/json/json.kt
@@ -1,8 +1,6 @@
 /*
- * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
-
-@file:Suppress("PropertyName")
 
 package org.jetbrains.amper.dependency.resolution.metadata.json
 
@@ -12,4 +10,3 @@ internal val json = Json {
     ignoreUnknownKeys = true
     isLenient = true
 }
-
