eb9d01b1463e56548f531093181522718cd345f2
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2025-03-18 16:07:59 +0100

    Remove Base.commonTestSettings
    They are no longer (if ever) used in the IDE plugin.

--- sources/frontend-api/src/org/jetbrains/amper/frontend/schema/root.kt
+++ sources/frontend-api/src/org/jetbrains/amper/frontend/schema/root.kt
@@ -1,10 +1,9 @@
 /*
- * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.amper.frontend.schema
 
-import org.jetbrains.amper.core.UsedInIdePlugin
 import org.jetbrains.amper.frontend.EnumMap
 import org.jetbrains.amper.frontend.Platform
 import org.jetbrains.amper.frontend.SchemaEnum
@@ -51,12 +50,6 @@ sealed class Base : SchemaNode() {
  * Common settings section.
  */
 val Base.commonSettings get() = settings[noModifiers]!!
-
-/**
- * Common test settings section.
- */
-@UsedInIdePlugin
-val Base.commonTestSettings get() = `test-settings`[noModifiers]!!
 
 class Template : Base()
 
