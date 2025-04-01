cc5a201bf231a84cccb59061d2ebd735e3c42b02
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-04-01 15:55:54 +0200

    [test] Un-ignore Gradle mobile tests
    These tests are not even external project tests!
    We probably didn't need to ignore them.

--- sources/amper-mobile-test/test/RunAndroidExamplesOnEmulatorsTestsGradle.kt
+++ sources/amper-mobile-test/test/RunAndroidExamplesOnEmulatorsTestsGradle.kt
@@ -5,18 +5,15 @@
 
 import androidUtils.AndroidBaseTest
 import org.junit.jupiter.api.Test
-import kotlin.test.Ignore
 
 class RunAndroidExamplesOnEmulatorsTestsGradle : AndroidBaseTest() {
 
     @Test
-    @Ignore("Unignore after relevant changes in `git.jetbrains.team/amper/amper-external-projects.git`")
     fun composeAndroidAppGradle() = testRunnerGradle(
         projectName = "compose-android",
     )
 
     @Test
-    @Ignore("Unignore after relevant changes in `git.jetbrains.team/amper/amper-external-projects.git`")
     fun composeAndroidMultiplatformAppGradle() = testRunnerGradle(
         projectName = "multiplatform",
         androidAppSubprojectName = "android-app",
