52ad50285ccd969f679628d0877519ccf04218ca
Space Automation <no-reply@automation.jetbrains.space>
2025-04-08 18:54:25 +0000

    Update Amper version to 0.7.0-dev-2642

--- examples-gradle/compose-multiplatform/settings.gradle.kts
+++ examples-gradle/compose-multiplatform/settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.7.0-dev-2636")
+    id("org.jetbrains.amper.settings.plugin").version("0.7.0-dev-2642")
 }
 
 include(":android-app")
--- examples-gradle/gradle-migration-kmp/settings.gradle.kts
+++ examples-gradle/gradle-migration-kmp/settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
 
 plugins {
     // apply the plugin:
-    id("org.jetbrains.amper.settings.plugin").version("0.7.0-dev-2636")
+    id("org.jetbrains.amper.settings.plugin").version("0.7.0-dev-2642")
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
 }
 
