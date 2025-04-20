1880fd5d6a95a72ed2c70c8d9872f8443cb9d240
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-04-20 02:25:37 +0200

    [concurrency] Simplify and fix StripedMutex
    So far, giving a stripe count that was not a power of 2
    actually had the effect of using a stripe count of the
    next power of 2. This doesn't respect the doc.
    Instead, we now enforce that the stripe count is a
    power of 2 and always respect it.
    
    This also simplifies the implementation.

--- sources/concurrency/src/org/jetbrains/amper/concurrency/StripedMutex.kt
+++ sources/concurrency/src/org/jetbrains/amper/concurrency/StripedMutex.kt
@@ -9,26 +9,46 @@ import kotlinx.coroutines.sync.withLock
 
 // initially from intellij:community/platform/build-scripts/downloader/src/striped.kt
 
-private const val MAX_POWER_OF_TWO = 1 shl Integer.SIZE - 2
-private const val ALL_SET = 0.inv()
-
 /**
- * A mutex that ensures exclusivity only within groups of callers (or "stripes").
- * It contains multiple mutexes, and each locking action requires some hash value to identify which stripe to lock.
+ * A mutex that ensures exclusivity within groups of callers (or "stripes").
+ * It contains [stripeCount] mutexes, and each locking action requires some hash value to identify which stripe to lock.
  *
- * This is useful when we don't want a global lock, but rather a lock associated with a specific resource that we can
- * identify with a hash. Using one mutex per resource would be a waste, so using stripe is a compromise.
+ * **The given [stripeCount] must be a power of 2.**
+ * This is because the mutexes are addressed using bitwise operations for performance.
+ *
+ * This kind of locking is useful when we want to avoid a global lock and instead protect only individual resources,
+ * but the number of resources is too large (or unknown) to create all mutexes up front (or at all).
+ * We could create mutexes dynamically in a concurrent map, but then we would pay the synchronization costs on the map
+ * lookup in addition to the mutex itself. We would also have no way to know when to clear the map, so it would also
+ * eventually blow the memory.
  */
 class StripedMutex(stripeCount: Int = 64) {
-    private val mask = if (stripeCount > MAX_POWER_OF_TWO) ALL_SET else (1 shl (Integer.SIZE - Integer.numberOfLeadingZeros(stripeCount - 1))) - 1
-    private val mutexes = Array(mask + 1) { Mutex() }
+    private val mask = stripeCount - 1
+    private val mutexes = Array(stripeCount) { Mutex() }
 
+    init {
+        require(stripeCount > 0) { "stripeCount must be positive, got $stripeCount" }
+        require(stripeCount and (stripeCount - 1) == 0) { "stripeCount must be a power of 2, got $stripeCount" }
+    }
+
+    /**
+     * Gets the [Mutex] corresponding to the given [hash].
+     *
+     * Invocations with the same hash are guaranteed to return the same [Mutex].
+     * Invocations with different hashes may or may not return the same [Mutex]
+     * (hashes in the same "stripe" share the same mutex).
+     *
+     * @see StripedMutex.withLock
+     */
     fun getMutex(hash: Int): Mutex = mutexes[hash and mask]
 }
 
 /**
  * Runs the given [action] under the lock identified by the given [hash].
+ *
  * Invocations with the same hash are guaranteed to use the same underlying [Mutex].
+ * Invocations with different hashes may or may not use the same [Mutex]
+ * (hashes in the same "stripe" share the same mutex).
  *
  * When [owner] is specified (non-null value) and this mutex is already locked with the same owner (same identity),
  * this function throws IllegalStateException (useful for debugging unexpected re-entries).
