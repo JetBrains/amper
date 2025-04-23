e571b7208c68f5a5366e093fe9dd84f81261b8e0
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2025-04-23 17:41:47 +0200

    Sort AOM diagnostic factories

--- sources/frontend/schema/src/org/jetbrains/amper/frontend/diagnostics/AomDiagnosticFactories.kt
+++ sources/frontend/schema/src/org/jetbrains/amper/frontend/diagnostics/AomDiagnosticFactories.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.amper.frontend.diagnostics
@@ -11,11 +11,11 @@ val AomModelDiagnosticFactories: List<AomModelDiagnosticFactory> = listOf(
 val AomSingleModuleDiagnosticFactories: List<AomSingleModuleDiagnosticFactory> = listOf(
     AndroidVersionShouldBeAtLeastMinSdkFactory,
     ComposeVersionWithDisabledCompose,
-    SerializationVersionWithDisabledSerialization,
-    UselessSettingValue,
     IncorrectSettingsLocation,
-    SigningEnabledWithoutPropertiesFileFactory,
+    KeystoreMustExistFactory,
     KeystorePropertiesDoesNotContainKeyFactory,
     MandatoryFieldInPropertiesFileMustBePresentFactory,
-    KeystoreMustExistFactory,
+    SerializationVersionWithDisabledSerialization,
+    SigningEnabledWithoutPropertiesFileFactory,
+    UselessSettingValue,
 )
