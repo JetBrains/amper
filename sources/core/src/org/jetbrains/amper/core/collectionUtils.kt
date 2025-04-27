c038422c39ce8b735af93bb94259d508c60891ad
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-04-28 00:24:35 +0200

    [infra] Delete unused mapStartAware

--- sources/core/src/org/jetbrains/amper/core/collectionUtils.kt
+++ sources/core/src/org/jetbrains/amper/core/collectionUtils.kt
@@ -1,11 +1,8 @@
 /*
- * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.amper.core
 
 fun <T> Collection<T>.forEachEndAware(block: (Boolean, T) -> Unit) =
     forEachIndexed { index, it -> if (index == size - 1) block(true, it) else block(false, it) }
-
-fun <T, V> Collection<T>.mapStartAware(block: (Boolean, T) -> V) =
-    mapIndexed { index, it -> if (index == 0) block(true, it) else block(false, it) }
