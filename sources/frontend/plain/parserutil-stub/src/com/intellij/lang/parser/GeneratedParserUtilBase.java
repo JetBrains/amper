c88d24facdb72d1509015e34110c10d74e007dca
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2025-04-28 17:08:41 +0200

    IJ Platform update to 251.23774: copy parser util

--- sources/frontend/plain/parserutil-stub/src/com/intellij/lang/parser/GeneratedParserUtilBase.java
+++ sources/frontend/plain/parserutil-stub/src/com/intellij/lang/parser/GeneratedParserUtilBase.java
@@ -1,6 +1,4 @@
-/*
- * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.lang.parser;
 
 import com.intellij.analysis.AnalysisBundle;
@@ -889,8 +887,8 @@ public class GeneratedParserUtilBase {
     }
 
     @Override
-    public @NotNull List<PsiBuilderImpl.ProductionMarker> getProductions() {
-      return ((PsiBuilderImpl)myDelegate).getProductions();
+    public @NotNull List<? extends Production> getProductions() {
+      return myDelegate.getProductions();
     }
   }
 
