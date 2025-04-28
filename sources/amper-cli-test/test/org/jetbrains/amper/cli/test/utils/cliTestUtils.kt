29fb9421c8235b841f65f9b17937d77f5cbc7f81
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-04-28 12:32:47 +0200

    [test] Move telemetry results helpers out of cliTestUtils

--- sources/amper-cli-test/test/org/jetbrains/amper/cli/test/utils/cliTestUtils.kt
+++ sources/amper-cli-test/test/org/jetbrains/amper/cli/test/utils/cliTestUtils.kt
@@ -4,15 +4,10 @@
 
 package org.jetbrains.amper.cli.test.utils
 
-import io.opentelemetry.api.common.AttributeKey
-import io.opentelemetry.sdk.trace.data.SpanData
 import kotlinx.coroutines.test.TestResult
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import org.jetbrains.amper.cli.test.AmperCliTestBase
-import org.jetbrains.amper.test.spans.FilteredSpans
-import org.jetbrains.amper.test.spans.SpansTestCollector
-import org.jetbrains.amper.test.spans.spansNamed
 import java.nio.file.Path
 import kotlin.io.path.div
 import kotlin.time.Duration.Companion.minutes
@@ -24,31 +19,3 @@ fun runSlowTest(testBody: suspend TestScope.() -> Unit): TestResult = runTest(ti
 //  User-visible artifacts should be placed in user-visible directories (use some convention).
 internal fun AmperCliTestBase.AmperCliResult.getTaskOutputPath(taskName: String): Path =
     buildOutputRoot / "tasks" / taskName.replace(':', '_')
-
-internal fun AmperCliTestBase.AmperCliResult.readTelemetrySpans(): SpansTestCollector {
-    return object : SpansTestCollector {
-        override val spans: List<SpanData> = telemetrySpans
-        override fun clearSpans() = throw UnsupportedOperationException("Cannot modify deserialized spans")
-    }
-}
-
-internal fun AmperCliTestBase.AmperCliResult.withTelemetrySpans(block: SpansTestCollector.() -> Unit) {
-    readTelemetrySpans().apply(block)
-}
-
-internal val SpansTestCollector.xcodebuildSpans: FilteredSpans
-    get() = spansNamed("xcodebuild")
-
-internal val SpansTestCollector.iosKotlinTests: FilteredSpans
-    get() = spansNamed("ios-kotlin-test")
-
-internal val SpansTestCollector.konancSpans: FilteredSpans
-    get() = spansNamed("konanc")
-
-internal val SpansTestCollector.xcodeProjectGenSpans
-    get() = spansNamed("xcode project generation")
-
-internal val SpansTestCollector.xcodeProjectManagementSpans
-    get() = spansNamed("xcode project management")
-
-internal val UpdatedAttribute = AttributeKey.booleanKey("updated")
---
+++ sources/amper-cli-test/test/org/jetbrains/amper/cli/test/utils/telemetryResults.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.jetbrains.amper.cli.test.utils
+
+import io.opentelemetry.api.common.AttributeKey
+import io.opentelemetry.sdk.trace.data.SpanData
+import org.jetbrains.amper.cli.test.AmperCliTestBase
+import org.jetbrains.amper.test.spans.FilteredSpans
+import org.jetbrains.amper.test.spans.SpansTestCollector
+import org.jetbrains.amper.test.spans.spansNamed
+
+internal fun AmperCliTestBase.AmperCliResult.readTelemetrySpans(): SpansTestCollector {
+    return object : SpansTestCollector {
+        override val spans: List<SpanData> = telemetrySpans
+        override fun clearSpans() = throw UnsupportedOperationException("Cannot modify deserialized spans")
+    }
+}
+
+internal fun AmperCliTestBase.AmperCliResult.withTelemetrySpans(block: SpansTestCollector.() -> Unit) {
+    readTelemetrySpans().apply(block)
+}
+
+internal val SpansTestCollector.xcodebuildSpans: FilteredSpans
+    get() = spansNamed("xcodebuild")
+
+internal val SpansTestCollector.iosKotlinTests: FilteredSpans
+    get() = spansNamed("ios-kotlin-test")
+
+internal val SpansTestCollector.konancSpans: FilteredSpans
+    get() = spansNamed("konanc")
+
+internal val SpansTestCollector.xcodeProjectGenSpans
+    get() = spansNamed("xcode project generation")
+
+internal val SpansTestCollector.xcodeProjectManagementSpans
+    get() = spansNamed("xcode project management")
+
+internal val UpdatedAttribute = AttributeKey.booleanKey("updated")
