79ed434ee749933bc6835a9ad4c4d9041b1d9edb
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-05-02 15:46:02 +0200

    [cli] AMPER-4312 Use runJava utility (with arg file) instead of raw BuildPrimitives.runProcessAndGetOutput
    Since we are passing the whole user classpath together with JUnit itself (and listeners)
    in the JVM args directly, the JUnit arg file no longer captures the biggest part of the
    command line, and we get "command line is too long" on Windows.
    
    This commit removes the JUnit arg file, and instead uses our runJava utility which puts
    _all_ arguments in the arg file, not just the JUnit ones.

--- sources/cli/src/org/jetbrains/amper/tasks/jvm/JvmTestTask.kt
+++ sources/cli/src/org/jetbrains/amper/tasks/jvm/JvmTestTask.kt
@@ -5,7 +5,6 @@
 package org.jetbrains.amper.tasks.jvm
 
 import com.github.ajalt.mordant.terminal.Terminal
-import org.jetbrains.amper.BuildPrimitives
 import org.jetbrains.amper.cli.AmperBuildOutputRoot
 import org.jetbrains.amper.cli.AmperProjectRoot
 import org.jetbrains.amper.cli.AmperProjectTempRoot
@@ -14,6 +13,7 @@ import org.jetbrains.amper.core.AmperUserCacheRoot
 import org.jetbrains.amper.core.UsedVersions
 import org.jetbrains.amper.core.downloader.Downloader
 import org.jetbrains.amper.core.extract.cleanDirectory
+import org.jetbrains.amper.core.telemetry.spanBuilder
 import org.jetbrains.amper.diagnostics.DeadLockMonitor
 import org.jetbrains.amper.engine.TaskGraphExecutionContext
 import org.jetbrains.amper.engine.TestTask
@@ -23,27 +23,24 @@ import org.jetbrains.amper.frontend.TaskName
 import org.jetbrains.amper.incrementalcache.ExecuteOnChangedInputs
 import org.jetbrains.amper.jvm.JdkDownloader
 import org.jetbrains.amper.processes.PrintToTerminalProcessOutputListener
+import org.jetbrains.amper.processes.runJava
 import org.jetbrains.amper.tasks.CommonRunSettings
+import org.jetbrains.amper.tasks.EmptyTaskResult
 import org.jetbrains.amper.tasks.TaskOutputRoot
 import org.jetbrains.amper.tasks.TaskResult
 import org.jetbrains.amper.tasks.TestResultsFormat
 import org.jetbrains.amper.telemetry.setListAttribute
-import org.jetbrains.amper.core.telemetry.spanBuilder
 import org.jetbrains.amper.telemetry.use
 import org.jetbrains.amper.test.FilterMode
 import org.jetbrains.amper.test.TestFilter
 import org.jetbrains.amper.test.wildcardsToRegex
 import org.slf4j.LoggerFactory
-import java.io.File
 import java.nio.file.Path
 import kotlin.io.path.createDirectories
-import kotlin.io.path.createTempFile
-import kotlin.io.path.deleteExisting
 import kotlin.io.path.div
 import kotlin.io.path.listDirectoryEntries
 import kotlin.io.path.outputStream
 import kotlin.io.path.pathString
-import kotlin.io.path.writeText
 
 class JvmTestTask(
     private val userCacheRoot: AmperUserCacheRoot,
@@ -67,7 +64,7 @@ class JvmTestTask(
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    override suspend fun run(dependenciesResult: List<TaskResult>, executionContext: TaskGraphExecutionContext): Result {
+    override suspend fun run(dependenciesResult: List<TaskResult>, executionContext: TaskGraphExecutionContext): TaskResult {
         // https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.10.1/junit-platform-console-standalone-1.10.1.jar
         val junitConsoleUrl = Downloader.getUriForMavenArtifact(
             mavenRepository = "https://repo1.maven.org/maven2",
@@ -82,7 +79,7 @@ class JvmTestTask(
             ?: error("${JvmCompileTask::class.simpleName} result is not found in dependencies")
         if (compileTask.classesOutputRoot.listDirectoryEntries().isEmpty()) {
             logger.warn("No test classes, skipping test execution for module '${module.userReadableName}'")
-            return Result()
+            return EmptyTaskResult
         }
 
         // test task depends on test jvm classpath task
@@ -96,8 +93,7 @@ class JvmTestTask(
         // TODO use maven instead of packing this in the distribution?
         val amperJUnitListenersJars = extractJUnitListenersClasspath()
 
-        // TODO settings!
-        val javaExecutable = JdkDownloader.getJdk(userCacheRoot).javaExecutable
+        val jdk = JdkDownloader.getJdk(userCacheRoot)
 
         cleanDirectory(taskOutputRoot.path)
 
@@ -121,12 +117,9 @@ class JvmTestTask(
             add("--details=summary") // disable default console tree output, just print the summary
         }
 
-        val junitArgsFile = createTempFile(tempRoot.path, "junit-args-", ".txt")
-        junitArgsFile.writeText(junitArgs.joinToString("\n") { arg ->
-            "\"${arg.replace("\\", "\\\\").replace("\"", "\\\"")}\""
-        })
-
         val jvmArgs = buildList {
+            add("-ea")
+
             if (commonRunSettings.testResultsFormat == TestResultsFormat.Pretty) {
                 add("-Dorg.jetbrains.amper.junit.listener.console.enabled=true")
                 // We don't use inherited IO when starting the test launcher process, so the Mordant Terminal library
@@ -148,62 +141,50 @@ class JvmTestTask(
             addAll(commonRunSettings.userJvmArgs)
         }
 
-        try {
-            // We pass both the user classpath and the "infra" classpath (JUnit itself and our listeners) together
-            // instead of using the separate --class-path option of the JUnit Console Launcher itself.
-            // This is intentional, to work around this JUnit issue: https://github.com/junit-team/junit5/issues/4469.
-            // In short, the separate --class-path option of the launcher was mostly meant as a convenience, but in
-            // some cases it is harmful. It is loaded in a separate class loader that is closed at the end of the tests,
-            // but before the test JVM shuts down. If the user code uses shutdown hooks (e.g., in testcontainers), they
-            // would not be able to load user classes anymore because of this.
-            val testJvmClasspath = listOf(junitConsole) + amperJUnitListenersJars + userTestRuntimeClasspath
-            val jvmCommand = listOf(
-                javaExecutable.pathString,
-                "-ea",
-                *jvmArgs.toTypedArray(),
-                "-cp",
-                testJvmClasspath.joinToString(File.pathSeparator),
-                "org.junit.platform.console.ConsoleLauncher",
-                "execute",
-                "@$junitArgsFile",
-            )
+        // We pass both the user classpath and the "infra" classpath (JUnit itself and our listeners) together
+        // instead of using the separate --class-path option of the JUnit Console Launcher itself.
+        // This is intentional, to work around this JUnit issue: https://github.com/junit-team/junit5/issues/4469.
+        // In short, the separate --class-path option of the launcher was mostly meant as a convenience, but in
+        // some cases it is harmful. It is loaded in a separate class loader that is closed at the end of the tests,
+        // but before the test JVM shuts down. If the user code uses shutdown hooks (e.g., in testcontainers), they
+        // would not be able to load user classes anymore because of this.
+        val testJvmClasspath = listOf(junitConsole) + amperJUnitListenersJars + userTestRuntimeClasspath
 
-            // TODO should be customizable?
-            // There is no way of knowing what the working dir should be for generated/unresolved test modules,
-            // the project root is a somewhat safe choice.
-            val workingDirectory = module.source.moduleDir ?: projectRoot.path
+        // TODO should be customizable?
+        // There is no way of knowing what the working dir should be for generated/unresolved test modules,
+        // the project root is a somewhat safe choice.
+        val workingDirectory = module.source.moduleDir ?: projectRoot.path
 
-            return spanBuilder("junit-platform-console-standalone")
-                .setAttribute("junit-platform-console-standalone", junitConsole.pathString)
-                .setAttribute("working-dir", workingDirectory.pathString)
-                .setListAttribute("tests-classpath", userTestRuntimeClasspath.map { it.pathString })
-                .setListAttribute("jvm-args", jvmArgs)
-                .setListAttribute("junit-args", junitArgs)
-                .use { span ->
-                    logger.info("Testing module '${module.userReadableName}' for platform '${platform.pretty}'...")
+        return spanBuilder("junit-platform-console-standalone")
+            .setAttribute("junit-platform-console-standalone", junitConsole.pathString)
+            .setAttribute("working-dir", workingDirectory.pathString)
+            .setListAttribute("tests-classpath", userTestRuntimeClasspath.map { it.pathString })
+            .setListAttribute("jvm-args", jvmArgs)
+            .setListAttribute("junit-args", junitArgs)
+            .use { span ->
+                logger.info("Testing module '${module.userReadableName}' for platform '${platform.pretty}'...")
 
-                    DeadLockMonitor.disable()
+                DeadLockMonitor.disable()
 
-                    val result = BuildPrimitives.runProcessAndGetOutput(
-                        workingDir = workingDirectory,
-                        command = jvmCommand,
-                        span = span,
-                        outputListener = PrintToTerminalProcessOutputListener(terminal),
-                    )
+                val result = jdk.runJava(
+                    workingDir = workingDirectory,
+                    mainClass = "org.junit.platform.console.ConsoleLauncher",
+                    jvmArgs = jvmArgs,
+                    classpath = testJvmClasspath,
+                    programArgs = listOf("execute") + junitArgs,
+                    tempRoot = tempRoot,
+                    outputListener = PrintToTerminalProcessOutputListener(terminal),
+                )
 
-                    // TODO exit code from junit launcher should be carefully become some kind of exit code for entire Amper run
-                    //  + one more interesting case: if we reported some failed tests to TeamCity, exit code of Amper should be 0,
-                    //  since the build will be failed anyway and it'll just have one more useless build failure about exit code
-                    if (result.exitCode != 0) {
-                        val meaning = if (result.exitCode == 2) " (no tests were discovered)" else ""
-                        userReadableError("JVM tests failed for module '${module.userReadableName}' with exit code ${result.exitCode}$meaning (see errors above)")
-                    }
-
-                    Result()
+                // TODO exit code from junit launcher should be carefully become some kind of exit code for entire Amper run
+                //  + one more interesting case: if we reported some failed tests to TeamCity, exit code of Amper should be 0,
+                //  since the build will be failed anyway and it'll just have one more useless build failure about exit code
+                if (result.exitCode != 0) {
+                    val meaning = if (result.exitCode == 2) " (no tests were discovered)" else ""
+                    userReadableError("JVM tests failed for module '${module.userReadableName}' with exit code ${result.exitCode}$meaning (see errors above)")
                 }
-        } finally {
-            junitArgsFile.deleteExisting()
-        }
+                EmptyTaskResult
+            }
     }
 
     private suspend fun extractJUnitListenersClasspath(): List<Path> {
@@ -254,6 +235,4 @@ class JvmTestTask(
     }
 
     private fun String.slashToDollar() = replace('/', '$')
-
-    class Result() : TaskResult
 }
