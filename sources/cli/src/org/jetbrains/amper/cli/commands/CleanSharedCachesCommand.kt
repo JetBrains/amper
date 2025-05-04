4299c697ea35b82cb4d87f33e29ebc8aa2b8a24a
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-05-04 19:08:46 +0200

    [cli] Remove unnecessary withBackend for CleanSharedCachesCommand

--- sources/cli/src/org/jetbrains/amper/cli/commands/CleanSharedCachesCommand.kt
+++ sources/cli/src/org/jetbrains/amper/cli/commands/CleanSharedCachesCommand.kt
@@ -1,12 +1,10 @@
 /*
- * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.amper.cli.commands
 
 import com.github.ajalt.clikt.core.Context
-import com.github.ajalt.clikt.core.terminal
-import org.jetbrains.amper.cli.withBackend
 import kotlin.io.path.deleteRecursively
 
 internal class CleanSharedCachesCommand : AmperSubcommand(name = "clean-shared-caches") {
@@ -14,11 +12,9 @@ internal class CleanSharedCachesCommand : AmperSubcommand(name = "clean-shared-c
     override fun help(context: Context): String = "Remove the Amper caches that are shared between projects"
 
     override suspend fun run() {
-        withBackend(commonOptions, commandName, terminal) { backend ->
-            val root = backend.context.userCacheRoot
-            logger.info("Deleting shared caches at ${root.path}...")
-            root.path.deleteRecursively()
-            logger.info("Deletion complete.")
-        }
+        val root = commonOptions.sharedCachesRoot
+        logger.info("Deleting shared caches at ${root.path}…")
+        root.path.deleteRecursively()
+        logger.info("Deletion complete.")
     }
 }
