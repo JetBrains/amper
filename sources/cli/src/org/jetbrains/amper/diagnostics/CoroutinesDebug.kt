7dcd0cf2deeb466d027e9a6c19ab91a5a1da3a05
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-05-06 00:34:47 +0200

    [cli] AMPER-4267 Do not crash on Windows Arm64 due to ByteBuddy incompatibility
    Setting up coroutine instrumentation is not a critical operation.
    If this fails, we should just print a warning.

--- sources/cli/src/org/jetbrains/amper/diagnostics/CoroutinesDebug.kt
+++ sources/cli/src/org/jetbrains/amper/diagnostics/CoroutinesDebug.kt
@@ -9,21 +9,34 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.debug.DebugProbes
 import org.jetbrains.amper.core.telemetry.spanBuilder
 import org.jetbrains.amper.telemetry.useWithoutCoroutines
+import org.slf4j.LoggerFactory
 
 object CoroutinesDebug {
+
+    private val logger = LoggerFactory.getLogger(CoroutinesDebug::class.java)
 
     @OptIn(ExperimentalCoroutinesApi::class)
     fun setupCoroutinesInstrumentation() {
         // TODO investigate the performance impact of the decoroutinator
         spanBuilder("Install stacktrace-decoroutinator").useWithoutCoroutines {
             // see https://github.com/Anamorphosee/stacktrace-decoroutinator#motivation
-            DecoroutinatorJvmApi.install()
+            try {
+                DecoroutinatorJvmApi.install()
+            } catch (e: Throwable) {
+                logger.warn("Failed to install stacktrace-decoroutinator: $e")
+            }
         }
 
         spanBuilder("Install coroutines debug probes").useWithoutCoroutines {
             // coroutines debug probes, required to dump coroutines
             DebugProbes.enableCreationStackTraces = false
-            DebugProbes.install()
+            try {
+                DebugProbes.install()
+            } catch (e: Throwable) {
+                // Always fails on Windows Arm64 because ByteBuddy doesn't support it:
+                // https://github.com/raphw/byte-buddy/issues/1336
+                logger.warn("Failed to install coroutines debug probes: $e")
+            }
         }
     }
 }
