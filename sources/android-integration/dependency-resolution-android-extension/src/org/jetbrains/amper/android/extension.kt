0bbbd9b1e1050188158c9fe7960f890932b694f3
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-05-09 13:16:08 +0200

    [android] Remove unused function

--- sources/android-integration/dependency-resolution-android-extension/src/org/jetbrains/amper/android/extension.kt
+++ sources/android-integration/dependency-resolution-android-extension/src/org/jetbrains/amper/android/extension.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 
@@ -9,27 +9,10 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import org.jetbrains.amper.core.extract.extractFileToLocation
-import org.jetbrains.amper.dependency.resolution.DependencyNode
-import org.jetbrains.amper.dependency.resolution.MavenDependencyNode
 import java.nio.file.Path
 import kotlin.io.path.div
 import kotlin.io.path.extension
 import kotlin.io.path.nameWithoutExtension
-
-
-/**
- * @return the list of paths which is pointed to extracted folders
- */
-suspend fun DependencyNode.extractAars(): List<Path> = coroutineScope {
-    distinctBfsSequence()
-        .filterIsInstance<MavenDependencyNode>()
-        .flatMap { it.dependency.files() }
-        .map { async { it.getPath() } }
-        .toList()
-        .awaitAll()
-        .filterNotNull()
-        .extractAars()
-}
 
 suspend fun List<Path>.extractAars(): List<Path> = coroutineScope {
     filter { it.extension == "aar" }
