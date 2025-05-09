7a9f2cad7413a7baa2134e869505152084bb8223
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-05-09 13:19:01 +0200

    [android] Remove unnecessary single-function module

--- sources/cli/src/org/jetbrains/amper/tasks/android/TransformAarExternalDependenciesTask.kt
+++ sources/cli/src/org/jetbrains/amper/tasks/android/TransformAarExternalDependenciesTask.kt
@@ -1,10 +1,13 @@
 /*
- * Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.amper.tasks.android
 
-import org.jetbrains.amper.android.extractAars
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import org.jetbrains.amper.core.extract.extractFileToLocation
 import org.jetbrains.amper.engine.Task
 import org.jetbrains.amper.engine.TaskGraphExecutionContext
 import org.jetbrains.amper.frontend.TaskName
@@ -14,6 +17,8 @@ import org.jetbrains.amper.tasks.ResolveExternalDependenciesTask
 import org.jetbrains.amper.tasks.TaskResult
 import java.nio.file.Path
 import kotlin.io.path.div
+import kotlin.io.path.extension
+import kotlin.io.path.nameWithoutExtension
 
 class TransformAarExternalDependenciesTask(
     override val taskName: TaskName,
@@ -32,4 +37,15 @@ class TransformAarExternalDependenciesTask(
     }
 
     class Result(override val compileClasspath: List<Path>) : TaskResult, AdditionalClasspathProvider
+}
+
+private suspend fun List<Path>.extractAars(): List<Path> = coroutineScope {
+    filter { it.extension == "aar" }
+        .map {
+            async {
+                val targetFolder = it.parent / it.nameWithoutExtension
+                extractFileToLocation(it, targetFolder)
+                targetFolder
+            }
+        }.awaitAll()
 }
