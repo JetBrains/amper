e411e749bafa10d52d449c90c76fb2603ca0c1f9
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2025-05-14 21:22:58 +0200

    [init] minor fix of init templates

--- sources/amper-project-templates/resources/templates/spring-boot-kotlin/src/org/jetbrains/amper/spring/Main.kt
+++ sources/amper-project-templates/resources/templates/spring-boot-kotlin/src/org/jetbrains/amper/spring/Main.kt
@@ -1,16 +1,12 @@
-/*
- * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
-
 package org.jetbrains.amper.spring
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    runApplication<Application>(*args)
 }
--- sources/amper-project-templates/resources/templates/spring-boot-kotlin/test/org/jetbrains/amper/spring/ExampleTest.kt
+++ sources/amper-project-templates/resources/templates/spring-boot-kotlin/test/org/jetbrains/amper/spring/ExampleTest.kt
@@ -1,7 +1,3 @@
-/*
- * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
-
 package org.jetbrains.amper.spring
 
 import org.springframework.boot.test.context.SpringBootTest
