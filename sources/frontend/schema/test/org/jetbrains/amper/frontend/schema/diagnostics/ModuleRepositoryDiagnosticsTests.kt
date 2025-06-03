a8daa76ed54d2e9090c2f4244d7d6ef8930cdcff
Alexey Barsov <alexey.barsov@jetbrains.com>
2025-06-03 19:08:57 +0200

    AMPER-503: Fixing ModuleRepositoryDiagnosticsTests

--- sources/frontend/schema/test/org/jetbrains/amper/frontend/schema/diagnostics/ModuleRepositoryDiagnosticsTests.kt
+++ sources/frontend/schema/test/org/jetbrains/amper/frontend/schema/diagnostics/ModuleRepositoryDiagnosticsTests.kt
@@ -8,23 +8,12 @@ import org.jetbrains.amper.core.messages.Level
 import org.jetbrains.amper.frontend.old.helper.TestBase
 import org.jetbrains.amper.frontend.schema.helper.diagnosticsTest
 import kotlin.io.path.Path
-import kotlin.io.path.div
 import kotlin.test.Test
 
 class ModuleRepositoryDiagnosticsTests : TestBase(Path("testResources/diagnostics/repositories")) {
 
     @Test
-    fun `implicitly resolvable maven local repository`() {
-        diagnosticsTest("repositories-mavenlocal-resolve-implicit", levels = arrayOf(Level.Warning))
-    }
-
-    @Test
-    fun `explicitly resolvable maven local repository`() {
-        diagnosticsTest("repositories-mavenlocal-resolve-explicit", levels = arrayOf(Level.Warning))
-    }
-
-    @Test
-    fun `no warning when maven local repository is correctly set with resolve false`() {
-        diagnosticsTest("repositories-mavenlocal-resolve-false", levels = arrayOf(Level.Warning))
+    fun `no warning for maven local repository`() {
+        diagnosticsTest("repositories-mavenlocal", levels = arrayOf(Level.Warning))
     }
 }
