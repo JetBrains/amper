be3537e8e7f1b986147f01fd1044355d5dad4caf
Alexey Barsov <alexey.barsov@jetbrains.com>
2025-06-17 11:55:02 +0200

    AMPER-4026: Supporting filtering dependencies graph by maven dependency coordinates, showing all paths this dependency is referenced by module

--- sources/amper-cli/src/org/jetbrains/amper/cli/commands/show/DependenciesCommand.kt
+++ sources/amper-cli/src/org/jetbrains/amper/cli/commands/show/DependenciesCommand.kt
@@ -7,12 +7,16 @@ package org.jetbrains.amper.cli.commands.show
 import com.github.ajalt.clikt.core.terminal
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.mordant.terminal.Terminal
+import org.jetbrains.amper.cli.AmperBackend
 import org.jetbrains.amper.cli.commands.AmperSubcommand
 import org.jetbrains.amper.cli.userReadableError
 import org.jetbrains.amper.cli.withBackend
 import org.jetbrains.amper.core.telemetry.spanBuilder
 import org.jetbrains.amper.dependency.resolution.DependencyNodeHolder
+import org.jetbrains.amper.dependency.resolution.MavenCoordinates
 import org.jetbrains.amper.dependency.resolution.ResolutionScope
+import org.jetbrains.amper.dependency.resolution.filterGraph
 import org.jetbrains.amper.frontend.AmperModule
 import org.jetbrains.amper.frontend.Platform
 import org.jetbrains.amper.frontend.dr.resolver.emptyContext
@@ -34,26 +38,29 @@ internal class DependenciesCommand: AmperSubcommand(name = "dependencies") {
         help = "Whether to include information about test dependencies or not, false by default")
         .flag(default = false)
 
+    private val filter by option("--filter",
+        help = "Coordinates of a particular dependency from the module dependencies graph. " +
+                "Only maven dependencies are supported at the moment in a format group:module. " +
+                "group defines a maven library group and module defines maven library module" +
+                "If the argument is specified, a subgraph of module dependencies that contains paths from root " +
+                "to the specified dependency will be presented as a result. " +
+                "If a dependency version is resolved based on a dependency constraint, " +
+                "the path from the root to that constraint will be included in the resulting subgraph as well. "
+    )
+
     override fun help(context: com.github.ajalt.clikt.core.Context): String = "Print the resolved dependencies graph of the module"
 
     override suspend fun run() {
         withBackend(commonOptions, commandName, terminal) { backend ->
             val terminal = backend.context.terminal
 
-            val modules = backend.modules()
-
-            val resolvedModule = module
-                ?.let { backend.resolveModule(it) }
-                ?: modules.singleOrNull()
-                ?: userReadableError(
-                    "There are several modules in the project. " +
-                            "Please specify one with '--module' argument.\n\n" +
-                            "Available application modules: ${backend.availableModulesString()}"
-                )
+            val resolvedModule = module.resolveModule(backend)
 
             val platformSetsToResolveFor = platforms
                 ?.let { listOf(resolvedModule.getModuleLeafPlatforms(it)) }
                 ?: resolvedModule.fragments.map { it.platforms }.distinct()
+
+            val mavenCoordinates = filter?.resolveFilter()
 
             val variantsToResolve = buildList {
                 platformSetsToResolveFor.forEach { platforms ->
@@ -89,18 +96,60 @@ internal class DependenciesCommand: AmperSubcommand(name = "dependencies") {
                 resolveSourceMoniker = "module ${resolvedModule.userReadableName}",
             )
 
-            terminal.println("Dependencies of module ${resolvedModule.userReadableName}: \n")
+            printDependencies(mavenCoordinates, terminal, resolvedModule, root, filter)
+        }
+    }
 
+    private fun printDependencies(
+        mavenCoordinates: MavenCoordinates?,
+        terminal: Terminal,
+        resolvedModule: AmperModule,
+        root: DependencyNodeHolder,
+        filter: String?,
+    ) {
+        if (mavenCoordinates == null) {
+            terminal.println("Dependencies of module ${resolvedModule.userReadableName}: \n")
             root.children.forEach {
                 terminal.println(it.prettyPrint())
             }
+        } else {
+            val rootsToPrint = root.children.mapNotNull {
+                filterGraph(mavenCoordinates.groupId, mavenCoordinates.artifactId, it)
+                    .takeIf { it.children.isNotEmpty() }
+            }
+            if (rootsToPrint.isEmpty()) {
+                terminal.println("Module doesn't depend on $filter")
+            } else {
+                terminal.println("Subgraphs of module dependencies showing where dependency on the library '$filter' comes from: \n")
+                rootsToPrint.forEach {
+                    terminal.println(it.prettyPrint())
+                }
+            }
         }
+    }
+
+    private fun String.resolveFilter(): MavenCoordinates {
+        val parts = this.split(":")
+        if (parts.size != 2) userReadableError("Option 'filter' supports maven coordinates in the format 'group:module' only.")
+        return MavenCoordinates(groupId = parts[0], artifactId = parts[1], version = null)
+    }
+
+    private fun String?.resolveModule(backend: AmperBackend): AmperModule {
+        val modules = backend.modules()
+        return this
+            ?.let { backend.resolveModule(it) }
+            ?: modules.singleOrNull()
+            ?: userReadableError(
+                "There are several modules in the project. " +
+                        "Please specify one with '--module' argument.\n\n" +
+                        "Available application modules: ${backend.availableModulesString()}"
+            )
     }
 
     /**
      * Get intersection of platforms specified in command line and leaf platforms of the module.
      * Report an error and exit immediately,
-     * If some input platforms are either unresolved or absent among module target platforms.
+     * if some input platforms are either unresolved or absent among module target platforms.
      */
     private fun AmperModule.getModuleLeafPlatforms(
         platformsString: String,
