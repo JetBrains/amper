0f14cb9bedccdee50788172953f434cf339cd201
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-07-22 22:05:57 +0200

    [frontend] Delete old entry points

--- sources/frontend/schema/src/org/jetbrains/amper/frontend/aomBuilder/readTemplate.kt
+++ sources/frontend/schema/src/org/jetbrains/amper/frontend/aomBuilder/readTemplate.kt
@@ -5,15 +5,11 @@
 package org.jetbrains.amper.frontend.aomBuilder
 
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiFile
 import org.jetbrains.amper.core.UsedInIdePlugin
 import org.jetbrains.amper.core.messages.ProblemReporter
-import org.jetbrains.amper.frontend.FrontendPathResolver
-import org.jetbrains.amper.frontend.ModelInit
 import org.jetbrains.amper.frontend.VersionCatalog
 import org.jetbrains.amper.frontend.catalogs.builtInCatalog
 import org.jetbrains.amper.frontend.catalogs.plus
-import org.jetbrains.amper.frontend.catalogs.substituteCatalogDependencies
 import org.jetbrains.amper.frontend.contexts.EmptyContexts
 import org.jetbrains.amper.frontend.project.AmperProjectContext
 import org.jetbrains.amper.frontend.schema.Template
@@ -27,25 +23,13 @@ import org.jetbrains.amper.frontend.tree.reading.readTree
 context(problemReporter: ProblemReporter)
 @UsedInIdePlugin
 fun AmperProjectContext.readEffectiveCatalogForTemplate(templateFile: VirtualFile): VersionCatalog? =
-    with(frontendPathResolver) {
-        readTemplate(templateFile = templateFile, projectVersionCatalog = projectVersionsCatalog)?.chosenCatalog
+    with(BuildCtx(pathResolver = frontendPathResolver, problemReporter = problemReporter)) {
+        val templateTree = readTree(file = templateFile, type = templateAType) ?: return null
+        val mergedTemplateTree = treeMerger.mergeTrees(templateTree)
+        val refiner = TreeRefiner()
+        // We can cast here, since we are not merging templates for now.
+        // NOTE: That will change when nested templated are allowed.
+        val noContextsTree = refiner.refineTree(mergedTemplateTree, EmptyContexts)
+        val noContextsTemplate = createSchemaNode<Template>(noContextsTree)
+        projectVersionsCatalog + noContextsTemplate.settings.builtInCatalog()
     }
-
-context(problemReporter: ProblemReporter, frontendPathResolver: FrontendPathResolver)
-internal fun readTemplate(
-    templateFile: VirtualFile,
-    projectVersionCatalog: VersionCatalog?,
-): ModelInit.TemplateHolder? = with(BuildCtx(pathResolver = frontendPathResolver, problemReporter = problemReporter)) {
-    val templateTree = readTree(templateFile, templateAType) ?: return null
-    val mergedTemplateTree = treeMerger.mergeTrees(templateTree)
-    val refiner = TreeRefiner()
-    // We can cast here, since we are not merging templates for now.
-    // NOTE: That will change when nested templated will be allowed.
-    val noContextsTree = refiner.refineTree(mergedTemplateTree, EmptyContexts)
-    val noContextsTemplate = createSchemaNode<Template>(noContextsTree)
-    val catalog = projectVersionCatalog + noContextsTemplate.settings.builtInCatalog()
-    val substituted = mergedTemplateTree.substituteCatalogDependencies(catalog)
-    val substitutedRefined = refiner.refineTree(substituted, EmptyContexts)
-    val readTemplate = createSchemaNode<Template>(substitutedRefined)
-    ModelInit.TemplateHolder(readTemplate, catalog)
-}
