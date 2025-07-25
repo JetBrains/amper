0e903191b2bd3b0b4d445fe9077115972ce46ba3
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-07-25 15:49:56 +0200

    [frontend] Remove unused FrontendApiBundle and the 2 unused messages

--- sources/frontend-api/src/org/jetbrains/amper/frontend/messageBundle.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
-
-package org.jetbrains.amper.frontend
-
-import org.jetbrains.amper.core.messages.MessageBundle
-
-object FrontendApiBundle : MessageBundle("messages.FrontendApiBundle")
