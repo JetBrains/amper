e8f8aaf2a62e532018b849c11e7fbe15e807ca68
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-07-25 18:42:39 +0200

    [frontend] Use proper trace for lombok defaults

--- sources/frontend/schema/src/org/jetbrains/amper/frontend/processing/lombok.kt
+++ sources/frontend/schema/src/org/jetbrains/amper/frontend/processing/lombok.kt
@@ -7,27 +7,28 @@ package org.jetbrains.amper.frontend.processing
 import org.jetbrains.amper.core.UsedVersions
 import org.jetbrains.amper.frontend.aomBuilder.BuildCtx
 import org.jetbrains.amper.frontend.api.DefaultTrace
+import org.jetbrains.amper.frontend.api.Trace
+import org.jetbrains.amper.frontend.api.valueBase
 import org.jetbrains.amper.frontend.schema.JavaAnnotationProcessingSettings
 import org.jetbrains.amper.frontend.schema.JavaSettings
 import org.jetbrains.amper.frontend.schema.MavenJavaAnnotationProcessorDeclaration
 import org.jetbrains.amper.frontend.schema.Module
 import org.jetbrains.amper.frontend.schema.Settings
-import org.jetbrains.amper.frontend.tree.MapLikeValue
 import org.jetbrains.amper.frontend.tree.Merged
-import org.jetbrains.amper.frontend.tree.Owned
 import org.jetbrains.amper.frontend.tree.asMapLike
 import org.jetbrains.amper.frontend.tree.syntheticBuilder
 
 context(buildCtx: BuildCtx)
 internal fun Merged.configureLombokDefaults(moduleCtxModule: Module): Merged =
     if (moduleCtxModule.settings.lombok.enabled) {
-        buildCtx.treeMerger.mergeTrees(listOfNotNull(asMapLike, buildCtx.lombokAnnotationProcessorDefaultsTree()))
+        val lombokDefault = DefaultTrace(computedValueTrace = moduleCtxModule.settings.lombok::enabled.valueBase)
+        buildCtx.treeMerger.mergeTrees(listOfNotNull(asMapLike, buildCtx.lombokAnnotationProcessorDefaultsTree(trace = lombokDefault)))
    } else {
         this
     }
 
-private fun BuildCtx.lombokAnnotationProcessorDefaultsTree() =
-    syntheticBuilder(types, DefaultTrace) {
+private fun BuildCtx.lombokAnnotationProcessorDefaultsTree(trace: Trace) =
+    syntheticBuilder(types, trace) {
         `object`<Module> {
             Module::settings {
                 Settings::java {
