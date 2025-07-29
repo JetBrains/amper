d1b466a672f8e2b42618c703b39e8567b8ba27ca
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-07-30 01:28:45 +0200

    [cli] Add bootstrap test that verifies Amper can test Amper to some extent

--- sources/test-integration/amper-cli-test/test/org/jetbrains/amper/cli/test/AmperBootstrapTest.kt
+++ sources/test-integration/amper-cli-test/test/org/jetbrains/amper/cli/test/AmperBootstrapTest.kt
@@ -13,9 +13,20 @@ class AmperBootstrapTest : AmperCliTestBase() {
 
     @Test
     fun `amper can build itself`() = runTest(timeout = 30.minutes) {
-        // We don't want to run the 'test' command because that would amount to run the whole test suite twice.
-        // Testing that the project compiles with the new sources is good enough, as the tests will be covered
-        // in the build of the MR that bumps Amper in Amper.
         runCli(projectRoot = Dirs.amperCheckoutRoot, "build")
+    }
+
+    @Test
+    fun `amper can launch smoke tests`() = runTest(timeout = 30.minutes) {
+        // We want to run some tests because it can discover problems with the test mechanism/classpath etc.
+        // We cannot run all tests because that would amount to running the whole test suite twice.
+        // All other tests will be covered anyway in the build of the MR that bumps Amper in Amper.
+        runCli(
+            projectRoot = Dirs.amperCheckoutRoot,
+            "test",
+            "-m",
+            "schema",
+            "--include-classes=org.jetbrains.amper.frontend.schema.ParserKtTest"
+        )
     }
 }
