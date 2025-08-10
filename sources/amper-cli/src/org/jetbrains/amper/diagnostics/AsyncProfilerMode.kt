064d55be3c5812f52432bde6ba74b008511ea5c4
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-08-11 00:56:50 +0200

    [cli] Minor refac of AsyncProfilerMode - separate functions

--- sources/amper-cli/src/org/jetbrains/amper/diagnostics/AsyncProfilerMode.kt
+++ sources/amper-cli/src/org/jetbrains/amper/diagnostics/AsyncProfilerMode.kt
@@ -12,7 +12,8 @@ import org.jetbrains.amper.core.system.Arch
 import org.jetbrains.amper.core.system.OsFamily
 import org.jetbrains.amper.stdlib.hashing.sha256String
 import org.slf4j.LoggerFactory
-import kotlin.io.path.createDirectories
+import java.nio.file.Path
+import kotlin.io.path.div
 import kotlin.io.path.fileSize
 import kotlin.io.path.isRegularFile
 import kotlin.io.path.pathString
@@ -20,56 +21,59 @@ import kotlin.io.path.writeBytes
 
 object AsyncProfilerMode {
     fun attachAsyncProfiler(logsDir: AmperBuildLogsRoot, buildOutputRoot: AmperBuildOutputRoot) {
-        val platformId = getPlatformId()
-        val ext = when (OsFamily.current) {
-            OsFamily.Windows -> ".dll"
-            OsFamily.Linux, OsFamily.FreeBSD, OsFamily.Solaris -> ".so"
-            OsFamily.MacOs -> ".dylib"
-        }
-        val name = "libasyncProfiler$ext"
-
-        val resourceName = "binaries/$platformId/$name"
-        val resourceStream = javaClass.classLoader.getResourceAsStream(resourceName)
-            ?: error("Resource '$resourceName' is not found in Amper classpath")
-        val libBytes = resourceStream.use { stream -> stream.readAllBytes() }
-        val libSha256 = libBytes.sha256String()
-
-        val libFile = buildOutputRoot.path.resolve("libasyncProfiler").resolve(libSha256).resolve(name)
-        // do not overwrite file unless absolutely necessary
-        // on Windows you can't overwrite an open file or a loaded library
-        if (!libFile.isRegularFile() || libFile.fileSize() != libBytes.size.toLong()) {
-            cleanDirectory(libFile.parent)
-            libFile.writeBytes(libBytes)
-        }
+        val profiler = getInstanceWithCachedLib(cacheDir = buildOutputRoot.path)
 
         val snapshotFile = logsDir.path.resolve("async-profiler-snapshot.jfr")
-            .also { it.parent.createDirectories() }
         val startCommand = "start,file=$snapshotFile,event=wall,interval=10ms,jfr,jfrsync=profile"
 
-        logger.info("Loading async profiler from $libFile, start command: $startCommand")
-
-        val asyncProfiler = AsyncProfiler.getInstance(libFile.pathString)
-        asyncProfiler.execute(startCommand)
-
+        logger.info("Starting async profiler with command: $startCommand")
+        profiler.execute(startCommand)
         logger.info("Async profiler started, snapshot file: $snapshotFile")
     }
 
-    private fun getPlatformId(): String {
-        val arch = Arch.current
-        return when (OsFamily.current) {
-            OsFamily.MacOs -> "macos"
+    private fun getInstanceWithCachedLib(cacheDir: Path): AsyncProfiler {
+        val name = getLibFilename()
+        val resourceName = "binaries/${getPlatformId()}/$name"
+        val resourceStream = javaClass.classLoader.getResourceAsStream(resourceName)
+            ?: error("Resource '$resourceName' is not found in Amper classpath")
 
-            OsFamily.Windows -> when (arch) {
-                Arch.X64 -> "windows"
-                Arch.Arm64 -> "windows-aarch64"
-            }
+        val libBytes = resourceStream.use { stream -> stream.readAllBytes() }
+        val libSha256 = libBytes.sha256String()
 
-            OsFamily.Linux, OsFamily.Solaris, OsFamily.FreeBSD -> when (arch) {
-                Arch.X64 -> "linux"
-                Arch.Arm64 -> "linux-aarch64"
-            }
+        val libFile = cacheDir / "libasyncProfiler" / libSha256 / name
+        // do not overwrite file unless absolutely necessary
+        // on Windows you can't overwrite an open file or a loaded library
+        if (!libFile.isRegularFile() || libFile.fileSize() != libBytes.size.toLong()) {
+            logger.info("Extracting async profiler lib to $libFile (only for the first run)")
+            cleanDirectory(libFile.parent)
+            libFile.writeBytes(libBytes)
         }
+        logger.info("Loading async profiler from $libFile")
+        return AsyncProfiler.getInstance(libFile.pathString)
     }
 
     private val logger = LoggerFactory.getLogger(javaClass)
+}
+
+private fun getLibFilename(): String {
+    val ext = when (OsFamily.current) {
+        OsFamily.Windows -> ".dll"
+        OsFamily.Linux, OsFamily.FreeBSD, OsFamily.Solaris -> ".so"
+        OsFamily.MacOs -> ".dylib"
+    }
+    return "libasyncProfiler$ext"
+}
+
+private fun getPlatformId(): String = when (OsFamily.current) {
+    OsFamily.MacOs -> "macos"
+    OsFamily.Windows -> when (Arch.current) {
+        Arch.X64 -> "windows"
+        Arch.Arm64 -> "windows-aarch64"
+    }
+    OsFamily.Linux,
+    OsFamily.Solaris,
+    OsFamily.FreeBSD -> when (Arch.current) {
+        Arch.X64 -> "linux"
+        Arch.Arm64 -> "linux-aarch64"
+    }
 }
