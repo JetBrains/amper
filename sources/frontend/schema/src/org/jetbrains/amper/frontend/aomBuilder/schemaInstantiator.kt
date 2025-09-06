3a13c623fd224a4231e6c27c3e6da6a4881097b9
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-09-06 23:23:14 +0200

    [frontend] Extract functions in schemaInstantiator and update docs

--- sources/frontend/schema/src/org/jetbrains/amper/frontend/aomBuilder/schemaInstantiator.kt
+++ sources/frontend/schema/src/org/jetbrains/amper/frontend/aomBuilder/schemaInstantiator.kt
@@ -25,9 +25,11 @@ import org.jetbrains.amper.frontend.types.toType
 import org.jetbrains.amper.problems.reporting.ProblemReporter
 
 /**
- * Instantiate requested [T] and fill its properties from the [node].
- * NOTE: if anything is reported in [MissingPropertiesHandler.onMissingRequiredPropertyValue], the resulting instance
- *  may throw on required property access.
+ * Instantiate the requested [T] and fill its properties from the [node].
+ *
+ * If the object cannot be created because it is missing or incomplete, this function returns `null`.
+ * This is the case when some required property is missing somewhere in the tree, for example.
+ * This ensures all returned objects are complete and respect their contract.
  *
  * @param missingPropertiesHandler provides a hook to report/handle missing required values.
  */
@@ -40,6 +42,16 @@ internal inline fun <reified T : SchemaNode> BuildCtx.createSchemaNode(
     }
 }
 
+/**
+ * Creates a value of the given [type] from the given tree [value].
+ *
+ * If the value cannot be created because it is missing or incomplete, this function returns `null`.
+ * This ensures all objects are complete and respect their contract.
+ *
+ * Note: this `null` marker only works because `null` is currently not denotable in our YAML.
+ *
+ * @param missingPropertiesHandler provides a hook to report/handle missing required values.
+ */
 context(missingPropertiesHandler: MissingPropertiesHandler)
 private fun createNode(
     type: SchemaType,
@@ -61,65 +73,9 @@ private fun createNode(
 
     return when (type) {
         is SchemaType.ScalarType if (value is ScalarValue<*>) -> value.value
-        is SchemaType.ListType if (value is ListValue<*>) -> value.children.map {
-            createNode(type.elementType, it, valuePath + "[]")
-        }.filter {
-            it != null || type.elementType.isMarkedNullable  // skip all the `null`s if the type is not nullable
-        }
-        is SchemaType.MapType if (value is Refined) -> value.children.associate {
-            val key = if (type.keyType.isTraceableWrapped) TraceableString(it.key, it.kTrace) else it.key
-            key to createNode(type.valueType, it.value, valuePath + it.key)
-        }.filterValues {
-            it != null || type.valueType.isMarkedNullable  // skip all the `null`s if the type is not nullable
-        }
-        is SchemaType.ObjectType if (value is Refined) -> {
-            val declaration = type.declaration
-            declaration.createInstance().apply {
-                @OptIn(InternalTraceSetter::class)
-                trace = value.trace
-                declaration.properties.forEach { property ->
-                    val propertyValuePath = valuePath + property.name
-                    val mapLikePropertyValue = value.refinedChildren[property.name]
-                    if (mapLikePropertyValue == null) {
-                        // Property is not mentioned at all
-                        if (property.isValueRequired()) {
-                            missingPropertiesHandler.onMissingRequiredPropertyValue(
-                                trace = value.trace,
-                                valuePath = propertyValuePath,
-                                keyTrace = null
-                            )
-                            // We don't allow incomplete objects
-                            return null
-                        }
-                        return@forEach
-                    }
-                    val node = createNode(
-                        type = property.type,
-                        value = mapLikePropertyValue.value,
-                        forProperty = property,
-                        propertyKeyTrace = mapLikePropertyValue.kTrace,
-                        valuePath = propertyValuePath
-                    )
-                    if (node == null) {
-                        if (property.isValueRequired()) {
-                            // We don't allow incomplete objects
-                            return null
-                        }
-                        // We don't create a `ValueHolder` for a nullable property in case of the `null` value,
-                        // because there may be an untraceable (`SchemaValueDelegate`-level) default,
-                        // e.g `Default.TransformedDependent`, which would not work because of the explicit ValueHolder.
-                        //
-                        // NOTE: This works because there is no explicit (user-intended) null support yet.
-                        // TODO: AMPER-4609
-                    } else {
-                        valueHolders[property.name] = ValueHolder(
-                            node,
-                            mapLikePropertyValue.value.trace,
-                        )
-                    }
-                }
-            }
-        }
+        is SchemaType.ListType if (value is ListValue<*>) -> createListNode(value, type, valuePath)
+        is SchemaType.MapType if (value is Refined) -> createMapNode(value, type, valuePath)
+        is SchemaType.ObjectType if (value is Refined) -> createObjectNode(value, type, valuePath)
         is SchemaType.VariantType if (value is Refined) -> {
             check(value.type in type.declaration.variants) {
                 "Type error: ${value.type} not among ${type.declaration.variants}"
@@ -131,6 +87,73 @@ private fun createNode(
             error("Type error: expected a `$type`, got: `$value`")
         }
     }
+}
+
+context(missingPropertiesHandler: MissingPropertiesHandler)
+private fun createListNode(value: ListValue<*>, type: SchemaType.ListType, valuePath: List<String>): List<Any?> =
+    value.children
+        .map { createNode(type.elementType, it, valuePath + "[]") }
+        .filter {
+            it != null || type.elementType.isMarkedNullable  // skip all the `null`s if the type is not nullable
+        }
+
+context(missingPropertiesHandler: MissingPropertiesHandler)
+private fun createMapNode(value: Refined, type: SchemaType.MapType, valuePath: List<String>): Map<Any, Any?> =
+    value.children
+        .associate {
+            val key = if (type.keyType.isTraceableWrapped) TraceableString(it.key, it.kTrace) else it.key
+            key to createNode(type.valueType, it.value, valuePath + it.key)
+        }
+        .filterValues {
+            it != null || type.valueType.isMarkedNullable  // skip all the `null`s if the type is not nullable
+        }
+
+context(missingPropertiesHandler: MissingPropertiesHandler)
+private fun createObjectNode(value: Refined, type: SchemaType.ObjectType, valuePath: List<String>): Any? {
+    val declaration = type.declaration
+    val newInstance = declaration.createInstance()
+    @OptIn(InternalTraceSetter::class)
+    newInstance.trace = value.trace
+
+    for (property in declaration.properties) {
+        val propertyValuePath = valuePath + property.name
+        val mapLikePropertyValue = value.refinedChildren[property.name]
+        if (mapLikePropertyValue == null) {
+            // Property is not mentioned at all
+            if (property.isValueRequired()) {
+                missingPropertiesHandler.onMissingRequiredPropertyValue(
+                    trace = value.trace,
+                    valuePath = propertyValuePath,
+                    keyTrace = null,
+                )
+                // We don't allow incomplete objects
+                return null
+            }
+            continue
+        }
+        val node = createNode(
+            type = property.type,
+            value = mapLikePropertyValue.value,
+            forProperty = property,
+            propertyKeyTrace = mapLikePropertyValue.kTrace,
+            valuePath = propertyValuePath,
+        )
+        if (node == null) {
+            if (property.isValueRequired()) {
+                // We don't allow incomplete objects
+                return null
+            }
+            // We don't create a `ValueHolder` for a nullable property in case of the `null` value,
+            // because there may be an untraceable (`SchemaValueDelegate`-level) default,
+            // e.g `Default.TransformedDependent`, which would not work because of the explicit ValueHolder.
+            //
+            // NOTE: This works because there is no explicit (user-intended) null support yet.
+            // TODO: AMPER-4609
+        } else {
+            newInstance.valueHolders[property.name] = ValueHolder(node, mapLikePropertyValue.value.trace)
+        }
+    }
+    return newInstance
 }
 
 /**
