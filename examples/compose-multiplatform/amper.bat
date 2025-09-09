48311cec395855bd2da7dd1690c9ad68abe4a9b2
Space Automation <no-reply@automation.jetbrains.space>
2025-09-09 19:44:32 +0000

    Update Amper version to 0.8.0-dev-3194

--- examples/compose-multiplatform/amper.bat
+++ examples/compose-multiplatform/amper.bat
@@ -16,9 +16,9 @@
 setlocal
 
 @rem The version of the Amper distribution to provision and use
-set amper_version=0.8.0-dev-3191
+set amper_version=0.8.0-dev-3194
 @rem Establish chain of trust from here by specifying exact checksum of Amper distribution to be run
-set amper_sha256=f74a200b54f17d8a0156b28146d7a285a41d2ec6a85fddafcb2742745e0bab5d
+set amper_sha256=ae0530fca1be6df99b53cd66d6ea2a0533050164825cf5be656bf926c3827883
 
 if not defined AMPER_DOWNLOAD_ROOT set AMPER_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined AMPER_JRE_DOWNLOAD_ROOT set AMPER_JRE_DOWNLOAD_ROOT=https:/
