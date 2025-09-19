14b2ce5cf75f6f356df9ace6d18d38496a4c8c13
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-09-19 17:31:53 +0200

    Update Spring petclinic example with new version from GitHub

--- examples/spring-petclinic-kotlin/test/org/springframework/samples/petclinic/PetclinicIntegrationTests.kt
+++ examples/spring-petclinic-kotlin/test/org/springframework/samples/petclinic/PetclinicIntegrationTests.kt
@@ -1,7 +1,3 @@
-/*
- * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
-
 package org.springframework.samples.petclinic
 
 import org.junit.jupiter.api.Test
--- examples/spring-petclinic-kotlin/test/org/springframework/samples/petclinic/vet/VetControllerTest.kt
+++ examples/spring-petclinic-kotlin/test/org/springframework/samples/petclinic/vet/VetControllerTest.kt
@@ -1,7 +1,3 @@
-/*
- * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
-
 package org.springframework.samples.petclinic.vet
 
 import org.hamcrest.xml.HasXPath.hasXPath
