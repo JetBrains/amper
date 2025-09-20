b89938622d65af9ce4ec6fea6dbed721eedfebe0
Fedor Ihnatkevich <fedor.ihnatkevich@jetbrains.com>
2025-09-20 17:57:14 +0200

    [infra] unpackedDist.task.yaml -> plugin

---
+++ build-sources/amper-distribution/src/buildUnpackedClasspath.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+import org.jetbrains.amper.Classpath
+import org.jetbrains.amper.Input
+import org.jetbrains.amper.Output
+import org.jetbrains.amper.TaskAction
+import org.jetbrains.amper.core.extract.cleanDirectory
+import org.jetbrains.amper.stdlib.hashing.sha256String
+import java.nio.file.Path
+import kotlin.io.path.copyTo
+import kotlin.io.path.createDirectory
+import kotlin.io.path.div
+import kotlin.io.path.extension
+import kotlin.io.path.name
+import kotlin.io.path.nameWithoutExtension
+import kotlin.io.path.pathString
+import kotlin.io.path.writeText
+
+@TaskAction
+fun buildUnpackedClasspath(
+    @Output targetDir: Path,
+    @Input baseClasspath: Classpath,
+    @Input extraClasspaths: Map<String, Classpath> = emptyMap(),
+    jarListFileName: String?,
+) {
+    cleanDirectory(targetDir)
+    (extraClasspaths + ("lib" to baseClasspath)).forEach { (name, paths) ->
+        val dir = (targetDir / name).createDirectory()
+        // some jars have the exact same filename even though they don't come from the same artifact
+        val alreadySeenFilenames = mutableSetOf<String>()
+        for (path in paths.resolvedFiles) {
+            val alreadyExists = !alreadySeenFilenames.add(path.name)
+            val filename = if (alreadyExists) {
+                "${path.nameWithoutExtension}-${path.pathString.sha256String().take(8)}.${path.extension}"
+            } else {
+                path.name
+            }
+            path.copyTo(dir.resolve(filename))
+        }
+    }
+    jarListFileName?.let { listFile ->
+        targetDir.resolve(listFile).writeText(baseClasspath.resolvedFiles.joinToString("\n") { it.name })
+    }
+}
