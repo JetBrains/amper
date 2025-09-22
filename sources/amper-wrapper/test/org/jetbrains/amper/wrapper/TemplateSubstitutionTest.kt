a05aeefaa9a9897c892b3c9966f74957d01fce54
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-09-22 22:52:34 +0200

    [wrapper] Simplify substitution code

--- sources/amper-wrapper/test/org/jetbrains/amper/wrapper/TemplateSubstitutionTest.kt
+++ sources/amper-wrapper/test/org/jetbrains/amper/wrapper/TemplateSubstitutionTest.kt
@@ -9,6 +9,7 @@ import java.nio.file.Path
 import kotlin.io.path.readText
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFails
 
 class TemplateSubstitutionTest {
     @TempDir
@@ -21,10 +22,51 @@ class TemplateSubstitutionTest {
         substituteTemplatePlaceholders(
             input = "some\r\ntemplate\n",
             outputFile = outputFile,
-            placeholder = "@",
-            values = emptyList(),
+            replacementRules = emptyList(),
         )
 
         assertEquals("some\r\ntemplate\n", outputFile.readText())
+    }
+
+    @Test
+    fun substituteTemplatePlaceholders_replacesPlaceholders_single() {
+        val outputFile = tempDir.resolve("output")
+
+        substituteTemplatePlaceholders(
+            input = "here is some @PLACEHOLDER@",
+            outputFile = outputFile,
+            replacementRules = listOf("@PLACEHOLDER@" to "value"),
+        )
+
+        assertEquals("here is some value", outputFile.readText())
+    }
+
+    @Test
+    fun substituteTemplatePlaceholders_replacesPlaceholders_multiple() {
+        val outputFile = tempDir.resolve("output")
+
+        substituteTemplatePlaceholders(
+            input = "here is @PLACEHOLDER1@\nand @PLACEHOLDER2@",
+            outputFile = outputFile,
+            replacementRules = listOf(
+                "@PLACEHOLDER1@" to "value1",
+                "@PLACEHOLDER2@" to "value2",
+            ),
+        )
+
+        assertEquals("here is value1\nand value2", outputFile.readText())
+    }
+
+    @Test
+    fun substituteTemplatePlaceholders_failsOnUnreplacedPlaceholders() {
+        val outputFile = tempDir.resolve("output")
+
+        assertFails {
+            substituteTemplatePlaceholders(
+                input = "here is some @PLACEHOLDER@",
+                outputFile = outputFile,
+                replacementRules = emptyList(),
+            )
+        }
     }
 }
