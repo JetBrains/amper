43c81557bbc04f409d8c4010b4c94bdca3f016f7
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2025-10-06 13:58:33 +0200

    [templates] update the compose-multiplatform template: AMPER-4724
    The previous version of the template is imported into IntelliJ incorrectly due to AMPER-4723 (which should be fixed separately anyway). But apart from that, we also do not need these dependencies in the shared module.
    Also, it is not necessary to use the AppCompatActivity, the regular ComponentActivity works well nowadays.
    
    The updated template is also more in line with the sample at `examples/compose-multiplatform`.

--- sources/amper-project-templates/resources/templates/compose-multiplatform/android-app/src/MainActivity.kt
+++ sources/amper-project-templates/resources/templates/compose-multiplatform/android-app/src/MainActivity.kt
@@ -2,10 +2,10 @@ package hello.world
 
 import Screen
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
