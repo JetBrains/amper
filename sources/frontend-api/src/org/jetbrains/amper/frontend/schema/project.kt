8f71a29c46e6787b380addc625f12a344bd3ce61
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2025-10-31 16:55:52 +0100

    [frontend] Better Markdown for documentation of Project.modules
    Split paragraphs using double \n and do new lines in the same paragraph using two spaces after.

--- sources/frontend-api/src/org/jetbrains/amper/frontend/schema/project.kt
+++ sources/frontend-api/src/org/jetbrains/amper/frontend/schema/project.kt
@@ -12,12 +12,12 @@ import org.jetbrains.amper.frontend.api.TraceableString
 class Project : SchemaNode() {
 
     @SchemaDoc(
-        "The relative paths or glob patterns defining the modules to include in this Amper project. " +
-                "The module at the root of the project is always included by default and doesn't need to be listed. " +
-                "Relative paths should be paths to the root directory of a module, directly containing the module file. " +
-                "For example, `./libs/util` will include the module defined by `./libs/util/module.yaml`." +
+        "The relative paths or glob patterns defining the modules to include in this Amper project.\n\n" +
+                "The module at the root of the project is always included by default and doesn't need to be listed.\n\n" +
+                "Relative paths should be paths to the root directory of a module, directly containing the module file.\n  " +
+                "For example, `./libs/util` will include the module defined by `./libs/util/module.yaml`.\n\n" +
                 "Glob patterns can be used to match multiple module directories in one expression. " +
-                "Only directories that actually contain a module file will be taken into account." +
+                "Only directories that actually contain a module file will be taken into account.\n  " +
                 "For example, `./libs/*` will include the modules defined by `./libs/foo/module.yaml` and " +
                 "`./libs/bar/module.yaml` (if these module files exist)."
     )
