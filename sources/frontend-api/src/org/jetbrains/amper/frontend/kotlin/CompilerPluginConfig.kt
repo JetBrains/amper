27d4b382b88e8090def44af7cecb277a10a5d916
Joffrey.Bion <joffrey.bion@jetbrains.com>
2025-11-19 23:50:27 +0100

    [cli] Remove obsolete handling of legacy Compose compiler plugin artifact
    Our minimum Kotlin version is now 2.1.20, so we will never have a Kotlin
    version that requires a legacy Compose compiler plugin.

--- sources/frontend-api/src/org/jetbrains/amper/frontend/kotlin/CompilerPluginConfig.kt
+++ sources/frontend-api/src/org/jetbrains/amper/frontend/kotlin/CompilerPluginConfig.kt
@@ -129,22 +129,11 @@ data class ComposeCompilerPluginConfig(val kotlinVersion: String) : CompilerPlug
         Option(name = "generateFunctionKeyMetaAnnotations", value = "true"),
         Option(name = "sourceInformation", value = "true"),
     )
-    override val mavenCoordinates = run {
-        val legacyComposeVersion = legacyComposeCompilerVersionFor(kotlinVersion)
-        if (legacyComposeVersion != null) {
-            CompilerPluginConfig.MavenCoordinates(
-                groupId = "org.jetbrains.compose.compiler",
-                artifactId = "compiler",
-                version = legacyComposeVersion,
-            )
-        } else {
-            CompilerPluginConfig.MavenCoordinates(
-                groupId = KOTLIN_GROUP_ID,
-                artifactId = "kotlin-compose-compiler-plugin-embeddable", // new artifact since 2.0.0-RC2
-                version = kotlinVersion, // the new artifact uses a version matching the Kotlin version
-            )
-        }
-    }
+    override val mavenCoordinates = CompilerPluginConfig.MavenCoordinates(
+        groupId = KOTLIN_GROUP_ID,
+        artifactId = "kotlin-compose-compiler-plugin-embeddable",
+        version = kotlinVersion,
+    )
 }
 
 data class LombokCompilerPluginConfig(val kotlinVersion: String) : CompilerPluginConfig {
@@ -155,49 +144,4 @@ data class LombokCompilerPluginConfig(val kotlinVersion: String) : CompilerPlugi
         artifactId = "kotlin-lombok-compiler-plugin-embeddable",
         version = kotlinVersion,
     )
-}
-
-/**
- * Returns the legacy Compose (multiplatform) compiler plugin version matching the given [kotlinVersion], or null if
- * the new Compose compiler artifact with the same version as Kotlin should be used (i.e. Kotlin >= 2.0.0-RC2).
- *
- * **IMPORTANT:** The user-defined Compose version is only for runtime libraries and the Compose Gradle plugin.
- * The Compose compiler has a different versioning scheme, with a mapping to the Kotlin compiler versions.
- *
- * ### Implementation note
- *
- * This mapping should not have to be updated anymore, because the Compose compiler is now part of the Kotlin repository
- * and is released with the same version as Kotlin itself.
- *
- * The original mapping in this function came from the tables in
- * [the official documentation](https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-compatibility-and-versioning.html),
- * and the
- * [mapping in the Gradle plugin](https://github.com/JetBrains/compose-multiplatform/blob/master/gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatibility.kt).
- */
-private fun legacyComposeCompilerVersionFor(kotlinVersion: String): String? = when (kotlinVersion) {
-    "1.7.10" -> "1.3.0"
-    "1.7.20" -> "1.3.2.2"
-    "1.8.0" -> "1.4.0"
-    "1.8.10" -> "1.4.2"
-    "1.8.20" -> "1.4.5"
-    "1.8.21" -> "1.4.7"
-    "1.8.22" -> "1.4.8"
-    "1.9.0-Beta" -> "1.4.7.1-beta"
-    "1.9.0-RC" -> "1.4.8-beta"
-    "1.9.0" -> "1.5.1"
-    "1.9.10" -> "1.5.2"
-    "1.9.20-Beta" -> "1.5.2.1-Beta2"
-    "1.9.20-Beta2" -> "1.5.2.1-Beta3"
-    "1.9.20-RC" -> "1.5.2.1-rc01"
-    "1.9.20-RC2" -> "1.5.3-rc01"
-    "1.9.20" -> "1.5.3"
-    "1.9.21" -> "1.5.4"
-    "1.9.22" -> "1.5.8.1"
-    "1.9.23" -> "1.5.13.5"
-    "1.9.24" -> "1.5.14"
-    "2.0.0-Beta1" -> "1.5.4-dev1-kt2.0.0-Beta1"
-    "2.0.0-Beta4" -> "1.5.9-kt-2.0.0-Beta4"
-    "2.0.0-Beta5" -> "1.5.11-kt-2.0.0-Beta5"
-    "2.0.0-RC1" -> "1.5.11-kt-2.0.0-RC1"
-    else -> null // since 2.0.0-RC2, the Compose compiler version matches the Kotlin version (/!\ different artifact)
 }
