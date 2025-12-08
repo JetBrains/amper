3a80ddd5a08bf04a10b747bf554a6681208d37a7
Space Automation <no-reply@automation.jetbrains.space>
2025-12-08 19:06:48 +0000

    Update Amper version to 0.10.0-dev-3535

--- misc/jvm/amper.bat
+++ misc/jvm/amper.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Amper distribution to provision and use
-set amper_version=0.10.0-dev-3532
+set amper_version=0.10.0-dev-3535
 @rem Establish chain of trust from here by specifying exact checksum of Amper distribution to be run
-set amper_sha256=892c974f54234ec9bef94655a7dc5c85c6e76027fe8f6f2e359424a091002337
+set amper_sha256=037f88528f3a35eca27ab593bf9aa79d81fe19ad3ccec327b399b2870fff1a67
 
 if not defined AMPER_DOWNLOAD_ROOT set AMPER_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined AMPER_JRE_DOWNLOAD_ROOT set AMPER_JRE_DOWNLOAD_ROOT=https:/
