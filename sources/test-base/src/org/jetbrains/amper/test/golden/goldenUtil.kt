c10a7df441db904d323f3fa0d9fd733001b72211
Alexey Barsov <alexey.barsov@jetbrains.com>
2025-12-12 15:43:49 +0100

    [dr] AMPER-4938 Introducing platform-aware golden files. It is required for Maven Profiles sopprt, because the same test might produce different graphs on different OS.

--- sources/test-base/src/org/jetbrains/amper/test/golden/goldenUtil.kt
+++ sources/test-base/src/org/jetbrains/amper/test/golden/goldenUtil.kt
@@ -4,9 +4,39 @@
 
 package org.jetbrains.amper.test.golden
 
+import org.jetbrains.amper.system.info.OsFamily
+import java.nio.file.Path
+import kotlin.io.path.exists
+
 fun String.trimTrailingWhitespacesAndEmptyLines(): String {
     return lines()
         .dropWhile { it.isBlank() }
         .dropLastWhile { it.isBlank() }
         .joinToString(separator = "\n") { it.trimEnd() }
+}
+
+/**
+ * Resolves the given file name inside the [Path].
+ * Tries to find a platform-specific variant of a golden file nearby
+ * (a file with the same name, but with additional platform-specific suffix at the end).
+ *
+ * @return platform-specific golden file path if it exists, or the original one if not found.
+ */
+fun Path.goldenFileOsAware(goldenFileBaseName: String): Path {
+    val osSuffix = when {
+        OsFamily.current.isWindows -> "-windows"
+        OsFamily.current.isMac -> "-mac"
+        OsFamily.current.isLinux -> "-linux"
+        else -> ""
+    }
+    val goldenFilePlatformSpecificName =
+        goldenFileBaseName.substringBeforeLast(".") +
+                osSuffix + "." +
+                goldenFileBaseName.substringAfterLast(".")
+
+    val goldenFile = resolve(goldenFilePlatformSpecificName)
+        .takeIf { it.exists() }
+        ?: resolve(goldenFileBaseName)
+
+    return goldenFile
 }
