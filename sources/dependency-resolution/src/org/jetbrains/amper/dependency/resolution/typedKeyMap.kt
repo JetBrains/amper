65cf9fe013ea5218b189e484ba3486372a46df4c
Joffrey.Bion <joffrey.bion@jetbrains.com>
2026-01-22 12:04:09 +0100

    [infra] Fix unchecked casts in DR TypedKeyMap
    We can actually perform checked casts using the KClass of the key.

--- sources/dependency-resolution/src/org/jetbrains/amper/dependency/resolution/typedKeyMap.kt
+++ sources/dependency-resolution/src/org/jetbrains/amper/dependency/resolution/typedKeyMap.kt
@@ -6,22 +6,24 @@ package org.jetbrains.amper.dependency.resolution
 
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.reflect.KClass
+import kotlin.reflect.cast
 
 inline fun <reified T : Any> Key(name: String): Key<T> = Key(name, T::class)
 
-data class Key<T : Any>(val name: String, private val kClass: KClass<T>)
+data class Key<T : Any>(val name: String, internal val kClass: KClass<T>)
 
 /**
  * Heterogeneous map that allows type-safe access by using [Key]s to retrieve/put values.
  */
-@Suppress("UNCHECKED_CAST")
 open class TypedKeyMap(protected val map: MutableMap<Key<*>, Any> = ConcurrentHashMap()) {
-    operator fun <T : Any> get(key: Key<T>): T? = map[key] as T?
+    operator fun <T : Any> get(key: Key<T>): T? = map[key]?.let { key.kClass.cast(it) }
 
     operator fun <T : Any> set(key: Key<T>, value: T) {
         map[key] = value
     }
 
-    fun <T : Any> computeIfAbsent(key: Key<T>, mappingFunction: (Key<T>) -> T): T =
-        map.computeIfAbsent(key, mappingFunction as (Key<*>) -> Any) as T
+    fun <T : Any> computeIfAbsent(key: Key<T>, mappingFunction: (Key<T>) -> T): T {
+        val value = map.computeIfAbsent(key) { mappingFunction(key) }
+        return key.kClass.cast(value)
+    }
 }
