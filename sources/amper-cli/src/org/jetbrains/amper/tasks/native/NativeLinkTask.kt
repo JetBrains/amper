0319a44e8421d0da2211e53cb68f3928112a7820
Léo Douglas Padilha <leodouglas@gmail.com>
2026-01-25 21:09:45 -0300

    Update sources/amper-cli/src/org/jetbrains/amper/tasks/native/NativeLinkTask.kt
    Co-authored-by: Joffrey Bion <joffrey.bion@gmail.com>

--- sources/amper-cli/src/org/jetbrains/amper/tasks/native/NativeLinkTask.kt
+++ sources/amper-cli/src/org/jetbrains/amper/tasks/native/NativeLinkTask.kt
@@ -109,13 +109,10 @@ internal class NativeLinkTask(
             .filterIsInstance<CinteropTask.Result>()
             .mapNotNull { it.compiledKlib }
 
-        println("Linking native binary for ${fragments.identificationPhrase()} with compiled klibs:\n" +
-                compiledKLibs.sorted().joinToString("\n").prependIndent("  ")
-        )
-
         val kotlinUserSettings = fragments.singleLeafFragment().serializableKotlinSettings()
 
-        logger.debug("native link '${module.userReadableName}' -- ${fragments.joinToString(" ") { it.name }}")
+        logger.debug("Linking native binary for ${fragments.identificationPhrase()} with compiled klibs:\n" +
+                compiledKLibs.sorted().joinToString("\n").prependIndent("  "))
 
         val entryPoints = if (module.type.isApplication()) {
             fragments.mapNotNull { it.settings.native?.entryPoint }.distinct()
