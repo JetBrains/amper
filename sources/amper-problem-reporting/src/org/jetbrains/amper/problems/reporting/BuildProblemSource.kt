966de68e4758cc2ba08ae4b7166e1151ebe8e0c5
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2026-03-02 17:10:51 +0100

    [frontend] Remove incorrect UsedInIdePlugin from MultipleLocationsBuildProblemSource

--- sources/amper-problem-reporting/src/org/jetbrains/amper/problems/reporting/BuildProblemSource.kt
+++ sources/amper-problem-reporting/src/org/jetbrains/amper/problems/reporting/BuildProblemSource.kt
@@ -1,10 +1,9 @@
 /*
- * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.amper.problems.reporting
 
-import org.jetbrains.amper.core.UsedInIdePlugin
 import java.nio.file.Path
 
 /**
@@ -29,8 +28,6 @@ class MultipleLocationsBuildProblemSource(
     val sources: List<FileBuildProblemSource>,
     val groupingMessage: String,
 ) : BuildProblemSource {
-    @UsedInIdePlugin
-
     constructor(
         vararg sources: FileBuildProblemSource,
         groupingMessage: String,
