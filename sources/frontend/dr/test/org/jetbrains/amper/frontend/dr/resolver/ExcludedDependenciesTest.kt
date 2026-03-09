cd96cc55ed813484421dcdf675078adc8e01521f
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2026-03-09 17:23:15 +0100

    [warnings] Fix unused variable

--- sources/frontend/dr/test/org/jetbrains/amper/frontend/dr/resolver/ExcludedDependenciesTest.kt
+++ sources/frontend/dr/test/org/jetbrains/amper/frontend/dr/resolver/ExcludedDependenciesTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.amper.frontend.dr.resolver
@@ -98,7 +98,7 @@ class ExcludedDependenciesTest: BaseModuleDrTest() {
         moduleYaml.writeText(patchedModuleYamlText)
 
         val aomPatched = getTestProjectModel(tmpProjectRoot)
-        val jvmAppDepsPatched = doTestByFile(
+        doTestByFile(
             testInfo,
             aomPatched,
             ResolutionInput(
